e73a97a1a8589859e0059cca90d22ae225b1d4f6
BALMES\sergi.marin <sergimarincanabate@gmail.com>
2018-01-30 17:59:27 +0100

    My first commit, scene test in progress

---
+++ Assets/Scripts/GitaHiro/Enemie.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemie : MonoBehaviour {
+    private float move;
+    public float vely = 5;
+    private Vector3 tmpPosition;
+    public float maxposy = 5.9f;
+    public Ball ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball.transform.position.x > 0)
+        {
+            if(ball.transform.position.y > transform.position.y)
+            {
+                move = 1.0f;
+            }else if (ball.transform.position.y < transform.position.y)
+            {
+                move = -1.0f;
+            }
+        }else
+        {
+            move = 0;
+        }
+
+        transform.Translate(0, move * vely * Time.deltaTime, 0);
+
+        if (transform.position.y > maxposy)
+        {
+            tmpPosition = new Vector3(transform.position.x, maxposy, transform.position.z);
+            transform.position = tmpPosition;
+        }
+
+        if (transform.position.y < -maxposy)
+        {
+            tmpPosition = new Vector3(transform.position.x, -maxposy, transform.position.z);
+            transform.position = tmpPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/GitaHiro/IroGuitar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IroGuitar : IMiniGame {
+    private GameManager gameManager;
+    public Baru baru;
+
+    void Awake()
+    {
+        //Init Pong
+        Debug.LogError("Change this Script for your own Script");
+    }
+
+    public override void beginGame()
+    {
+        //Pong Begins
+        Debug.Log(this.ToString() + " game Begin");
+        baru.enableBall = true;
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+        baru.init(gm); 
+    }
+
+    public override string ToString()
+    {
+        return "Pong by Jandujar";
+    }
+}
