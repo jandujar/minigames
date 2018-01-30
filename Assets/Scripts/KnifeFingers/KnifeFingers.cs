1ccd2f7f8c731ad2d2f503790501307625c8d26e
BALMES\adrian.jerez <adrijerez9@gmail.com>
2018-01-30 18:20:23 +0100

    Knife movement

---
+++ Assets/Scripts/KnifeFingers/KnifeFingers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeFingers : IMiniGame {
+    void Awake()
+    {
+        Debug.LogError("Change this Script for your own Script");
+    }
+
+    public override void beginGame()
+    {
+        //Pong Begins
+        Debug.Log(this.ToString() + " game Begin");
+        //ball.enableBall = true;
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        //this.gameManager = gm;
+        //ball.init(gm);
+    }
+
+    public override string ToString()
+    {
+        return "Knife finger roulette by Adrian (idea by Didac)";
+    }
+}
