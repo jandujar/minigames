0b011bf44549cc8011672c068de12f14d7cfbc09
BALMES\fabio.scarcella <toxedits@gmail.com>
2018-02-06 17:47:25 +0100

    Ball moving and game loading correctly

---
+++ Assets/Scripts/Equilibrio/BallEquilibrio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEquilibrio : MonoBehaviour {
+
+    //public Vector2 movement;
+    public bool enableBall = false;
+    private GameManager gameManager;
+    private float maxVel = 0.5f;
+
+    private float move = 0.0f;
+    private float currentSpeed = 0.0f;
+
+
+    public void init(GameManager gm)
+    {
+        gameManager = gm;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+        move = Input.GetAxis("Horizontal");
+        currentSpeed = (move* Time.deltaTime) / 2;
+        transform.Translate(move / 6, 0, 0);
+        Debug.Log(currentSpeed);
+	}
+}
