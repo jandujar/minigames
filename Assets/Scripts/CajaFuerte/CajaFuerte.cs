f6ef45b433b1d2baafca8e8a142a16eb1812dfee
ESCOBAR_PALAUDARIAS <marc.escobar@balmes.escolapia.net>
2018-02-06 18:02:11 +0100

    caja fuerte

---
+++ Assets/Scripts/CajaFuerte/CajaFuerte.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaFuerte : IMiniGame
+{
+
+
+
+    private GameManager gameManager;
+    public Slider slider;
+
+    void Awake()
+    {
+        //Init Pong
+        Debug.LogError("Change this Script for your own Script");
+    }
+
+    public override void beginGame()
+    {
+        //Pong Begins
+        Debug.Log(this.ToString() + " game Begin");
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+        slider.init(gm);
+
+    }
+
+    public override string ToString()
+    {
+        return "Caja Fuerte Miau";
+    }
+
+}
+
---
+++ Assets/Scripts/CajaFuerte/Slider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slider : MonoBehaviour {
+
+    private GameManager gameManager;
+    public GameObject nail;
+    private int direction;
+
+    public void init(GameManager gm)
+    {
+        gameManager = gm;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        direction = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        nail.transform.Rotate(0, 0, Time.deltaTime * 10 * direction, Space.World);
+
+        if (nail.transform.rotation.z >= 120 && nail.transform.rotation.z <= 130)
+        {
+            direction = -1;
+            Debug.Log("menos");
+
+        }else if(nail.transform.rotation.z <= -115 && nail.transform.rotation.z >= -125)
+        {
+            direction = 1;
+            Debug.Log("mas");
+        }
+
+        
+
+    }
+}
