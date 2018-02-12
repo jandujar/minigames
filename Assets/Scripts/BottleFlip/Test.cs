ec215c2040653e140481c913c8e84a8d39bbd3fe
Marc Alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-02-12 16:04:53 +0100

    Scripts del otro proyecto minigames y sprites importados

---
+++ Assets/Scripts/BottleFlip/Test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Test : MonoBehaviour
+{
+
+    /*private float minForce = 0f;
+    public float maxForce = 100f;
+    public Rigidbody rb;
+    public Slider sl;
+    public GameObject objToMove;
+    // Use this for initialization
+    void Start()
+    {
+        sl.minValue = minForce;
+        sl.maxValue = maxForce;
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rotateNow();
+    }
+    public void rotateNow()
+    {
+        //sl.value = minForce;
+
+        //rb.AddForce(transform.up * force);
+        objToMove.transform.Rotate(0, 0, minForce);
+
+    }*/
+}
