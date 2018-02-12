61793a9d7957993c4e5fe0476c37f4277a769077
Carles -HOME <carles.condeminas@gmail.com>
2018-02-12 13:17:47 +0100

    canvas shows at time

--- Assets/Scripts/MSegada/M_Segada_CCR.cs
+++ Assets/Scripts/MSegada/M_Segada_CCR.cs
@@ -20,17 +20,18 @@ public class M_Segada_CCR : IMiniGame
     public enum QTResponse { Null, Success, Fail };                 //win/fail game
     public QTResponse qtResponse = QTResponse.Null;                 //win condition not defined for now
 
-    public float CountTimer = 2f;                                   //duration of the event
-    public float DelayTimer = 0f;                                   //delay before event starts
+    [SerializeField]
+    private float CountTimer = 0.3f;                                //duration of the event
+    [SerializeField]
+    private float DelayTimer = 0f;                                  //delay before event starts
 
-
-    public List<KeyCode> Buttons = new List<KeyCode>();
     public bool isRandom = false;
     private int ran = 0;
 
     public override void beginGame()
     {
-        StartCoroutine(StateChange());
+        NEWcanvas.SetActive(true);                                  //our game shows 
+        StartCoroutine(StateChange());                              //win/loose condition starts
         //throw new NotImplementedException();
     }
 
@@ -40,51 +41,37 @@ public class M_Segada_CCR : IMiniGame
         gameManager = gm;
     }
 
-    private IEnumerator StateChange()
+   
+    // Use this for initialization
+    void Start () {
+        DelayTimer = UnityEngine.Random.Range(1, 5);                //random time of waiting to the start of the event
+        NEWcanvas.SetActive(false);
+    }
+	 private IEnumerator StateChange()
     {
-        qtState = QTState.Delay;
-        // Wait for the Delay if any delay at all.
-        yield return new WaitForSeconds(DelayTimer);
-        // If we want a random button we use Random.Range, otherwise we set it to the first button in the list!
-        if (isRandom)
-        {
-            ran = UnityEngine.Random.Range(0, Buttons.Count);
-        }
-        else
-        {
-            ran = 0;
-        }
-        // This line below is only for Debug Purposes
-        print(Buttons[ran]);
-        // This adds the selected button to the display GUI and makes it uppercase instead of lowercase.
-        DisplayBox.GetComponent<Text>().text = Buttons[ran].ToString().ToUpper();
+        qtState = QTState.Delay;        
+        yield return new WaitForSeconds(DelayTimer);                // Wait for the Delay        
+        DisplayBox.GetComponent<Text>().text = "QUICK! JUMP!";      //shows text to JUMP
         qtState = QTState.Ongoing;
         yield return new WaitForSeconds(CountTimer);
-        // If the timer is over and the event isn't over? Fix it! because most likely they failed.
-        if (qtState == QTState.Ongoing)
+       
+        if (qtState == QTState.Ongoing)                             //count.timer gets to 0
         {
             qtResponse = QTResponse.Fail;
             gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
-            qtState = QTState.Done;
-            // We ready the gui text for the next event/time.
+            qtState = QTState.Done;           
             DisplayBox.GetComponent<Text>().text = string.Empty;
         }
     }
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
         if (qtState == QTState.Ongoing)
         {            
-                if (Input.anyKeyDown)
+                if (Input.anyKeyDown)                               //we press ANY key during the OnGoing state
                 {
                     qtState = QTState.Done;
                     qtResponse = QTResponse.Success;
-                    // We ready the gui text for the next event/time.
-                    //ButtonDisplay.text = string.Empty;
+                    
                     DisplayBox.GetComponent<Text>().text = "WIN";
                     PassBox.GetComponent<Text>().text = "WIN";
                     StopCoroutine(StateChange());
