3d54d109ad1949d3ce834be114a3ccf85b50c1ec
DESKTOP-CD7K19P\Fabio <toxedits@gmail.com>
2018-02-13 11:03:43 +0100

    Game working with Cylinder and Rectangle, still have to do the win and lose condition and put sprites

--- Assets/Scripts/Equilibrio/BallEquilibrio.cs
+++ Assets/Scripts/Equilibrio/BallEquilibrio.cs
@@ -12,6 +12,9 @@ public class BallEquilibrio : MonoBehaviour {
     private float move = 0.0f;
     private float currentSpeed = 0.0f;
 
+    private Vector3 angles;
+
+    public GameObject cube;
 
     public void init(GameManager gm)
     {
@@ -23,9 +26,13 @@ public class BallEquilibrio : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
+        angles = transform.rotation.eulerAngles;
+        angles.z = cube.transform.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(angles);
+
         move = Input.GetAxis("Horizontal");
         currentSpeed = (move* Time.deltaTime) / 2;
         transform.Translate(move / 6, 0, 0);
-        Debug.Log(currentSpeed);
-	}
+
+    }
 }
