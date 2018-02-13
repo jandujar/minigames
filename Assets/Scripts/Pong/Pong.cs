2cbf402d534e42283392ce6479e8d6a3c65c7038
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-02-13 16:37:26 +0100

    cambio del fichero pong.cs

---
+++ Assets/Scripts/Pong/Pong.cs
@@ -0,0 +1,34 @@
+﻿ using System;
+ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class Pong : IMiniGame
+{
+     private GameManager gameManager;
+     public Ball ball;
+ 
+     void Awake()
+     {
+         //Init Pong
+         Debug.LogError("Change this Script for your own Script");
+     }
+ 
+     public override void beginGame()
+     {
+         //Pong Begins
+         Debug.Log(this.ToString() + " game Begin");
+         ball.enableBall = true;
+     }
+ 
+     public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+     {
+         this.gameManager = gm;
+         ball.init(gm); 
+     }
+ 
+     public override string ToString()
+     {
+         return "Pong by Jandujar";
+     }
+ }
