814181bdf49f28c51f313ce4c3aa36e6b507fc17
Raul Reche <raulreche9@gmail.com>
2018-02-14 13:01:01 +0100

    changes

--- Assets/Scripts/VeganKnows/QuickTimeEvent.cs
+++ Assets/Scripts/VeganKnows/QuickTimeEvent.cs
@@ -32,12 +32,12 @@ public class QuickTimeEvent : MonoBehaviour {
             if (QTEGen == 1)
             {
                 WaitingForKey = 1;
-                DisplayBox.GetComponent<Text>().text = "[E]";
+                DisplayBox.GetComponent<Text>().text = "A";
             }
             if (QTEGen == 2)
             {
                 WaitingForKey = 1;
-                DisplayBox.GetComponent<Text>().text = "[R]";
+                DisplayBox.GetComponent<Text>().text = "B";
             }
             StartCoroutine(LoseTime());
         }
@@ -69,34 +69,28 @@ public class QuickTimeEvent : MonoBehaviour {
     IEnumerator KeyPressing(){
         QTEGen = 3;
         if (CorrectKey == 1){
-            PassBox.GetComponent<Text>().text = "PASS1";
-            yield return new WaitForSeconds(1.5f);
-            CorrectKey = 0;
-            PassBox.GetComponent<Text>().text = "";
-            DisplayBox.GetComponent<Text>().text = "";
-            yield return new WaitForSeconds(1.5f);
+            PassBox.GetComponent<Text>().text = "TU SI QUE SABES, BRO";
+            yield return new WaitForSeconds(3.0f);
             WaitingForKey = 0;
         }
         if (CorrectKey == 2)
         {
-            PassBox.GetComponent<Text>().text = "FAIL2";
+            PassBox.GetComponent<Text>().text = "ASESINO!!";
+            yield return new WaitForSeconds(3.0f);
             //Condicion de derrota
             Debug.Log("Loser");
             gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
         }
         if (CorrectKey == 3)
         {
-            PassBox.GetComponent<Text>().text = "PASS3";
-            yield return new WaitForSeconds(1.5f);
-            CorrectKey = 0;
-            PassBox.GetComponent<Text>().text = "";
-            DisplayBox.GetComponent<Text>().text = "";
-            yield return new WaitForSeconds(1.5f);
+            PassBox.GetComponent<Text>().text = "¿NO TENDRIAS UN POCO DE SALSA?";
+            yield return new WaitForSeconds(3.0f);
             WaitingForKey = 0;
         }
         if (CorrectKey == 4)
         {
-            PassBox.GetComponent<Text>().text = "FAIL4";
+            PassBox.GetComponent<Text>().text = "LO VERDE PARA LOS CONEJOS";
+            yield return new WaitForSeconds(3.0f);
             //Condicion de derrota
             Debug.Log("Loser");
             gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
