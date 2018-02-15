486723644b418cd3f4afec1e04e3a944afc4f307
Serch-PC\Serch <sergi.lopez@balmes.epiaedu.cat>
2018-02-15 12:33:01 +0100

    Push into NumberList all the random numbers

--- Assets/Scripts/MathematicalOperations/MathematicalOperations.cs
+++ Assets/Scripts/MathematicalOperations/MathematicalOperations.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MathematicalOperations : IMiniGame
 {
     private GameManager gameManager;
-    public GameObject numbers;
-    public GameObject operations;
-    private Transform[] aNumbers;
+    public static MathematicalOperations instance = null;
+    private List<string> listNumbers = new List<string>();
+    private int countNumbers = 4;
 
+    
     void Awake()
     {
         //Init Game
         Debug.LogError("Change this Script for your own Script");
-        
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+       
 
     }
 
@@ -21,8 +27,6 @@ public class MathematicalOperations : IMiniGame
     {
         //Game Begins
         Debug.Log(this.ToString() + " game Begin");
-       /* numbers.SetActive(true);
-        operations.SetActive(false);*/
     }
 
     public override void initGame(MiniGameDificulty difficulty, GameManager gm)
@@ -35,4 +39,48 @@ public class MathematicalOperations : IMiniGame
     {
         return "Mathematical Operations by Sergi López";
     }
+
+    public void setNumberInList(string nameID, int value)
+    {
+        listNumbers.Add(nameID + ", " + value);
+
+        Debug.Log("Length ListNumbers: " + listNumbers.Count);
+
+        if (listNumbers.Count == 4)
+        {
+           invertOrderList();
+
+            foreach (string i in listNumbers)
+            {
+                Debug.Log("ListNumbers: " + i);
+            }
+        }
+        
+    }
+
+    public List<string> getOperation()
+    {
+        return listNumbers;
+    }
+
+    public int getCountNumbers()
+    {
+        return countNumbers--;
+    }
+
+    private void invertOrderList()
+    {
+        //Coge el primero y segundo 
+        string aux = listNumbers[0]; 
+        string aux2 = listNumbers[1];
+
+        //Subtituye los 2 últimos por los 2 primeros
+        listNumbers[0] = listNumbers[3];
+        listNumbers[1] = listNumbers[2];
+
+        //Subtituye los 2 primeros por los 2 últimos
+        listNumbers[2] = aux2;
+        listNumbers[3] = aux;
+    }
+    
 }
--- Assets/Scripts/MathematicalOperations/RandomNumbers.cs
+++ Assets/Scripts/MathematicalOperations/RandomNumbers.cs
@@ -6,14 +6,18 @@ public class RandomNumbers : MonoBehaviour {
     
     public  GameObject[] aNumbers = new GameObject[10];
     int rand = 0;
-    void Awake()
+    string nameID = "Num"; 
+    void Start()
     {
         //Init Game    
 
         rand = (int)Random.Range(0f, 10f);
-        Debug.Log(rand);
 
          aNumbers[rand].gameObject.SetActive(true);
+
+        MathematicalOperations.instance.setNumberInList(nameID + MathematicalOperations.instance.getCountNumbers(), rand);
     }
+
+    
     
 }
