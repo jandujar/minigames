bf9ab57fe8839d3a1cc2361093a84131218be27a
joey013 <dracojoy@gmail.com>
2018-02-26 01:46:26 +0100

    First Commit

---
+++ Assets/Scenes/BurgerBuilder/ReadMe.md
@@ -0,0 +1,3 @@
+Burger Builder
+Created by Joel Reverté Naharro (dracojoy@gmail.com)
+2018/02/12
---
+++ Assets/Scripts/BurgerBuilder/BurgerBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurgerBuilder : IMiniGame{
+    private GameManager gameManager;
+    private int timeToLoose = 0;
+    public Transform canvas;
+    public AudioClip backgroundMusic;
+    private AudioSource source;    
+
+    public override void beginGame()
+    {
+        //Pong Begins
+        Debug.Log(this.ToString() + " game Begin");
+        canvas.gameObject.SetActive(true);
+        source = GetComponent<AudioSource>();
+        source.PlayOneShot(backgroundMusic, 1f);
+
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+        StartCoroutine(LooseTime());
+        
+    }
+
+    IEnumerator LooseTime()
+    {
+        if (timeToLoose <= 13)
+        {
+            yield return new WaitForSeconds(1);
+            timeToLoose++;
+            StartCoroutine(LooseTime());
+        }
+        else {
+            canvas.gameObject.SetActive(false);
+            gameManager.EndGame(MiniGameResult.LOSE);
+        }
+        
+
+    }
+
+    public override string ToString()
+    {
+        return "Duck Shooter by DarkJoe";
+    }
+}
