0364d8646d9d1cd2f992d8ffc7f0bfca66c61d1d
Didac_HomePC <didacaltesa@gmail.com>
2018-02-26 20:02:52 +0100

    Basic Gameplay

--- Assets/Scripts/FlappyBird/ParallaxManager.cs
+++ Assets/Scripts/FlappyBird/ParallaxManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ParallaxManager : MonoBehaviour {
 	public float[] parallaxSpeed = new float[4];
 	 
-    public GameObject[] elements = new GameObject[4];
+    public GameObject[] elements = new GameObject[5];
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +15,11 @@ public class ParallaxManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		MoveBackground ();
+		for (int i = 0; i < 2; i++) {
+			if (elements [i].transform.position.x < -39.27f) {
+				elements [i].transform.position = new Vector3(-19.65f,elements [i].transform.position.y,elements [i].transform.position.z);
+			}
+		}
 	}
 
     public void MoveBackground() {
