9d995bb93b9bf1398642a5c54642250336902e6c
BALMES\sergi.marin <sergimarincanabate@gmail.com>
2018-02-26 19:47:53 +0100

    Game base selfies

---
+++ Assets/Scripts/SerufiPanikku/SerufiImgManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerufiImgManager : MonoBehaviour {
+
+    public List<Material> selfieToPrint;
+
+    Material gameMaterial;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        selfieToPrint = new List<Material>();
+        gameMaterial = this.GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public IEnumerator changeImgTest()
+    {
+        while(true)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            Debug.Log("Img 1");
+            gameMaterial = selfieToPrint[1];
+            yield return new WaitForSecondsRealtime(1f);
+            Debug.Log("Img 0");
+            gameMaterial = selfieToPrint[0];
+        }        
+    }
+}
