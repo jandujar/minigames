6ecf00cd6d8a35ad5cba70478badb3b6badc0374
joey013 <dracojoy@gmail.com>
2018-02-26 11:44:37 +0100

    Bread Movement Started

---
+++ Assets/Scripts/BurgerBuilder/BreadMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadMovement : MonoBehaviour {
+
+    public Transform rightLimit;
+    public Transform leftLimit;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
