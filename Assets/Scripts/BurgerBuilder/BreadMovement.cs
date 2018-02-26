a0ed27cfe04dc287b53ae691a8033be3c979c400
REVERTE_NAHARRO <joel.reverte@balmes.escolapia.net>
2018-02-26 18:36:52 +0100

    Bread Movement

--- Assets/Scripts/BurgerBuilder/BreadMovement.cs
+++ Assets/Scripts/BurgerBuilder/BreadMovement.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class BreadMovement : MonoBehaviour {
 
+
+    [Header("Floats")]
+    public float moveSpd = 100f;
+    private float move;
+
+    //Vectors
+    private Vector3 tmpPosition;
+
+    [Header("Transform")]
     public Transform rightLimit;
     public Transform leftLimit;
 
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        move = InputManager.Instance.GetAxisHorizontal();
+
+        transform.Translate(move * moveSpd * Time.deltaTime, 0, 0);
+
+        if (transform.position.x > rightLimit.position.x)
+        {
+            tmpPosition = new Vector3(rightLimit.position.x, transform.position.y, transform.position.z);
+            transform.position = tmpPosition;
+        }
+        if (transform.position.x < leftLimit.position.x)
+        {
+            tmpPosition = new Vector3(leftLimit.position.x, transform.position.y, transform.position.z);
+            transform.position = tmpPosition;
+        }
+    }
 }
---
+++ Assets/Scripts/BurgerBuilder/IngredientMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientMovement : MonoBehaviour {
+
+    [Header("Floats")]
+    public float moveSpd = 0.5f;
+
+    [Header("Transforms")]
+    public Transform leftLimit;
+    public Transform rightLimit;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * moveSpd);
+        if (transform.position.x == rightLimit.position.x || transform.position.x == leftLimit.position.x) {
+            moveSpd = -moveSpd;
+        }
+	}
+}
