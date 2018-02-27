334e691b3e599f870dc7ed7ced640680f27db551
AnarKi <randomless81@gmail.com>
2018-02-27 20:04:02 +0100

    programando al diferentes piezas (raycast para saber cual es el out)

---
+++ Assets/Scripts/Bomb/BombPiece.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombPiece : MonoBehaviour {
+
+    //PUBLIC
+    public GameObject[] arrayInOut;
+    public SpriteRenderer[] arraySpriteInOut;
+
+    //PRIVATE
+    private float move;
+    private int randomOut;
+
+
+	// Use this for initialization
+	void Start () {
+
+        if (this.name == "PieceStart1" || this.name == "PieceStart4")
+        {
+            arrayInOut[0].gameObject.name = "in";
+            randomOut = Random.Range(1, 4);
+            arrayInOut[randomOut].gameObject.name = "out";
+            arraySpriteInOut[randomOut].enabled = true;
+        }
+        if (this.name == "PieceStart2" || this.name == "PieceStart3")
+        {
+            arrayInOut[0].gameObject.name = "in";
+            randomOut = Random.Range(1, 7);
+            arrayInOut[randomOut].gameObject.name = "out";
+            arraySpriteInOut[randomOut].enabled = true;
+        }
+
+	}
+	
+
+}
