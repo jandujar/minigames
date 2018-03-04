91d1da4d177787dd2f093de4d7ad102f87f8b661
Carles -HOME <carles.condeminas@gmail.com>
2018-03-04 18:24:06 +0100

    maceta player can move

---
+++ Assets/Scripts/Maceta/Lanzador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lanzador : MonoBehaviour {
+    private float move;
+    public float vely = 5;
+    private Vector3 tmpPosition;
+    public float maxposx = 5.9f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move = InputManager.Instance.GetAxisHorizontal();
+        
+        transform.Translate( move * vely * Time.deltaTime,0, 0);
+
+        if (transform.position.x > maxposx)
+        {
+            tmpPosition = new Vector3(maxposx, transform.position.y, transform.position.z);
+            transform.position = tmpPosition;
+        }
+
+        if (transform.position.x < -maxposx)
+        {
+            tmpPosition = new Vector3(-maxposx, transform.position.y, transform.position.z);
+            transform.position = tmpPosition;
+        }
+    }
+}
