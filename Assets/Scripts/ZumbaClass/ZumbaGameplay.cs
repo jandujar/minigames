f115149e6edb2fb2ddb60bb2ab26ab4f3cc2cd7c
Fabio Scarcella <toxedits@gmail.com>
2018-03-05 16:44:50 +0100

    Implemented some sounds to the game

--- Assets/Scripts/ZumbaClass/ZumbaGameplay.cs
+++ Assets/Scripts/ZumbaClass/ZumbaGameplay.cs
@@ -8,77 +8,111 @@ public class ZumbaGameplay : MonoBehaviour {
     public GameObject x;
     public GameObject y;
 
+    public GameObject winSound;
+    public GameObject loseSound;
+    public GameObject missSound;
+
     AudioSource myAudio;
 
+    private bool hasBeenClicked;
     private bool aClicked;
     private bool bClicked;
     private bool xClicked;
     private bool yClicked;
     private int num;
 
+    private int lifes;
+
 
     // Use this for initialization
     void Start () {
+        lifes = 3;
+        hasBeenClicked = false;
         myAudio = GetComponent<AudioSource>();
         StartCoroutine(GameBegins());
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //CORRECT
-        if(Input.GetButtonDown("Fire1") && num == 0)
-        {
-            a.SetActive(false);
-            Debug.Log("Yea");
-        }
 
-        if (Input.GetButtonDown("Fire2") && num == 1)
+        if(lifes > 0)
         {
-            b.SetActive(false);
-            Debug.Log("Yea");
-        }
+            //CORRECT
+            if (Input.GetButtonDown("Fire1") && num == 0)
+            {
+                a.GetComponent<AudioSource>().Play();
+                hasBeenClicked = true;
+                a.SetActive(false);
+            }
 
-        if (Input.GetButtonDown("Fire3") && num == 2)
-        {
-            x.SetActive(false);
-            Debug.Log("Yea");
-        }
-        if (Input.GetButtonDown("Fire4") && num == 3)
-        {
-            y.SetActive(false);
-            Debug.Log("Yea");
-        }
-        //WRONG
-        if (Input.GetButtonDown("Fire1") && num != 0)
-        {
-            Debug.Log("Wrong");
-        }
-        if (Input.GetButtonDown("Fire2") && num != 1)
-        {
-            Debug.Log("Wrong");
-        }
+            if (Input.GetButtonDown("Fire2") && num == 1)
+            {
+                b.GetComponent<AudioSource>().Play();
+                hasBeenClicked = true;
+                b.SetActive(false);
+            }
 
-        if (Input.GetButtonDown("Fire3") && num != 2)
-        {
-            Debug.Log("Wrong");
+            if (Input.GetButtonDown("Fire3") && num == 2)
+            {
+                x.GetComponent<AudioSource>().Play();
+                hasBeenClicked = true;
+                x.SetActive(false);
+            }
+            if (Input.GetButtonDown("Fire4") && num == 3)
+            {
+                y.GetComponent<AudioSource>().Play();
+                hasBeenClicked = true;
+                y.SetActive(false);
+            }
+
+            //WRONG
+            if (Input.GetButtonDown("Fire1") && num != 0)
+            {
+                hasBeenClicked = false;
+                Debug.Log("Wrong");
+            }
+            if (Input.GetButtonDown("Fire2") && num != 1)
+            {
+                hasBeenClicked = false;
+                Debug.Log("Wrong");
+            }
+
+            if (Input.GetButtonDown("Fire3") && num != 2)
+            {
+                hasBeenClicked = false;
+                Debug.Log("Wrong");
+            }
+            if (Input.GetButtonDown("Fire4") && num != 3)
+            {
+                hasBeenClicked = false;
+                Debug.Log("Wrong");
+            }
         }
-        if (Input.GetButtonDown("Fire4") && num != 3)
+        else
         {
-            Debug.Log("Wrong");
-        }
+            Debug.Log("LOSER");
+            loseSound.GetComponent<AudioSource>().Play();
+        }        
     }
 
     IEnumerator GameBegins()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(4);
         myAudio.Play();
         for(int i = 0; i < 12; i++)
         {
             yield return new WaitForSeconds(0.2f);
+            hasBeenClicked = false;
             getOneButton();
             yield return new WaitForSeconds(0.3f);
             PutActiveFalse();
             num = 10;
+            returnIfClicked(hasBeenClicked);
+        }
+        yield return new WaitForSeconds(0.5f);
+        if(lifes > 0)
+        {
+            winSound.GetComponent<AudioSource>().Play();
         }
 
     }
@@ -124,6 +158,15 @@ public class ZumbaGameplay : MonoBehaviour {
                 break;
             default:
                 break;
+        }
+    }
+
+    private void returnIfClicked(bool clicked)
+    {
+        if (!clicked)
+        {
+            missSound.GetComponent<AudioSource>().Play();
+            lifes--;
         }
     }
 }
