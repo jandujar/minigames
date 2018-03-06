18eacc7bafd3b9ef604cf0ea88f855a8ced54a34
Luka Giacco <devourerofthecosmos@hotmail.com>
2018-03-06 16:26:52 +0100

    Cambiado el Tag para que no haya conflictos

--- Assets/Scripts/Matrix/SPlayer.cs
+++ Assets/Scripts/Matrix/SPlayer.cs
@@ -32,7 +32,7 @@ public class SPlayer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.collider.tag == "Bullet")
+        if (other.collider.tag == "Finish")
         {
             gm.EndGame(IMiniGame.MiniGameResult.LOSE);
         }
