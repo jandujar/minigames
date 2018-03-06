a1be1366e26d2aa0cb803eddcfc4a847fe494587
BALMES\roger.quesada <rogerk.1996@gmail.com>
2018-03-06 16:54:01 +0100

    Tries to kill mosquito implemented

--- Assets/Scripts/Mosquito/EspatulaMove.cs
+++ Assets/Scripts/Mosquito/EspatulaMove.cs
@@ -3,79 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EspatulaMove : MonoBehaviour {
+public class EspatulaMove : MonoBehaviour
+{
 
-	public float speed = 1;
-	public float pivotOffset = 5;
-	public float maxVertical, maxHorizontal;
-	public float maxPositionOffset;
-	public bool attacking = false;
-	Animator anim;
+    public AudioClip mosquitoSplat;
+    private AudioSource source;
+    public bool splatCanPlay;
+    public float speed = 1;
+    public float pivotOffset = 5;
+    public float maxVertical, maxHorizontal;
+    public float maxPositionOffset;
+    public bool attacking = false;
+    Animator anim;
 
-	GameManager gameManager;
+    GameManager gameManager;
 
-	public int tries = 3;
+    public int tries = 3;
 
-	public Text txt;
-	public void init(GameManager gm)
-	{
-	
-		gameManager = gm;
-		txt.text = tries.ToString();
-	}
+    public Text txt;
+    public void init(GameManager gm)
+    {
 
-	// Use this for initialization
-	void Start () {
-		maxVertical = Camera.main.orthographicSize;    
-		maxVertical -= maxPositionOffset;
+        gameManager = gm;
+        txt.text = tries.ToString();
+        source = GetComponent<AudioSource>();
+        splatCanPlay = false;
+    }
 
-		maxHorizontal = maxVertical * Screen.width / Screen.height;
-		anim = GetComponent<Animator> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!attacking) {
-			Vector2 movement = new Vector2 (InputManager.Instance.GetAxisHorizontal (), InputManager.Instance.GetAxisVertical ());
-			Vector3 newPosition = transform.position + (Vector3)movement * speed * Time.deltaTime;
-			if (newPosition.x  > maxHorizontal) {
-				newPosition.x = maxHorizontal;
-			} else if (newPosition.x < -maxHorizontal) {
-				newPosition.x = -maxHorizontal;
-			}
+    // Use this for initialization
+    void Start()
+    {
+        maxVertical = Camera.main.orthographicSize;
+        maxVertical -= maxPositionOffset;
 
-			if (newPosition.y > maxVertical+pivotOffset) {
-				newPosition.y = pivotOffset +maxVertical;
-			} else if (newPosition.y < pivotOffset-maxVertical) {
-				newPosition.y = pivotOffset-maxVertical;
-			}
+        maxHorizontal = maxVertical * Screen.width / Screen.height;
+        anim = GetComponent<Animator>();
+        //source = GetComponent<AudioSource>();
 
-			transform.position = newPosition;
 
-			if (InputManager.Instance.GetButtonDown (InputManager.MiniGameButtons.BUTTON1)) {
-				Attack();
-			}
-		}
-	}
+    }
 
-	void Attack(){
-		attacking = true;
-		anim.SetTrigger ("attack");
-	}
-	public void StopAttack(){
-		attacking = false;
-		tries--;
-		txt.text = tries.ToString();
+    // Update is called once per frame
+    void Update()
+    {
+        if (!attacking)
+        {
+            Vector2 movement = new Vector2(InputManager.Instance.GetAxisHorizontal(), InputManager.Instance.GetAxisVertical());
+            Vector3 newPosition = transform.position + (Vector3)movement * speed * Time.deltaTime;
+            if (newPosition.x > maxHorizontal)
+            {
+                newPosition.x = maxHorizontal;
+            }
+            else if (newPosition.x < -maxHorizontal)
+            {
+                newPosition.x = -maxHorizontal;
+            }
 
-		if (tries <= 0) {
-			gameManager.EndGame (IMiniGame.MiniGameResult.LOSE);
-		}
-	}
+            if (newPosition.y > maxVertical + pivotOffset)
+            {
+                newPosition.y = pivotOffset + maxVertical;
+            }
+            else if (newPosition.y < pivotOffset - maxVertical)
+            {
+                newPosition.y = pivotOffset - maxVertical;
+            }
 
-	void OnTriggerEnter2D(Collider2D other){
-	
-		if (other.gameObject.CompareTag ("Player")) {
-			gameManager.EndGame (IMiniGame.MiniGameResult.WIN);
-		}
-	}
+            transform.position = newPosition;
+
+            if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
+            {
+                Attack();
+            }
+            txt.text = "you have  " + tries.ToString() + " tries to kill the mosquito";
+        }
+    }
+
+    void Attack()
+    {
+        attacking = true;
+        anim.SetTrigger("attack");
+        splatCanPlay = true;
+    }
+    public void StopAttack()
+    {
+        attacking = false;
+        tries--;
+        txt.text = tries.ToString();
+
+        if (tries <= 0)
+        {
+            gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+         
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+           source.PlayOneShot(mosquitoSplat, .5f);
+                splatCanPlay = false;
+           
+            //source.PlayOneShot(MosquitoSplat, .5f);
+            gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
+        }
+    }
+
+
 }
