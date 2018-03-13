af497c82b66225fdca78334d6f6650bd9d3f26c9
DOMINGUEZ_REIG <antoni.dominguez@balmes.escolapia.net>
2018-03-13 15:43:38 +0100

    smoke Random programmed

---
+++ Assets/Scripts/Brocheta/Brocheta.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class Brocheta : IMiniGame
+{
+
+    private GameManager gameManager;
+    public ParticleSystem[] smokes;
+    bool actSmoke;
+    int rand;
+    int lastRand;
+
+
+
+    void Start()
+    {
+        actSmoke = true;
+    }
+    void Update()
+    {
+        rand = UnityEngine.Random.Range(0, smokes.Length);
+        Debug.Log(rand);
+
+        if (actSmoke)
+        {
+            smokes[rand].Play();
+            StartCoroutine(startSmoke());
+        }
+
+    }
+    
+    IEnumerator startSmoke()
+    {
+        actSmoke = false;
+        lastRand = rand;
+        yield return new WaitForSeconds(4);
+        smokes[lastRand].Stop();
+        actSmoke = true;
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+    }
+
+    public override void beginGame()
+    {
+    }
+
+    //gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
+    //gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+
+}
