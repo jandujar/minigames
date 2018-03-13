a13be0cf6efa6d1e9fc36171fe9760a26ce10722
Fabio Scarcella <toxedits@gmail.com>
2018-03-13 18:28:52 +0100

    Primitives cubes are spawning with rigidbody, but has some bugs...

---
+++ Assets/Scripts/RollingBall/RollingBallInit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBallInit : IMiniGame {
+
+
+	public override void beginGame()
+    {
+        //Pong Begins
+        Debug.Log(this.ToString() + " game Begin!");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+
+    }
+}
---
+++ Assets/Scripts/RollingBall/SpawnRandomRolling.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandomRolling : MonoBehaviour {
+
+	//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	private ArrayList objectsToSpawn;
+	public Vector3 pos1; //middle
+	public Vector3 pos2; //left
+	public Vector3 pos3; //right
+	public GameObject parent; 
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(CallSpawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator CallSpawn(){
+		yield return new WaitForSeconds(4);
+		for(int i = 0; i < 15; ++i){
+			Spawn();
+			yield return new WaitForSeconds(1);
+		}
+	}
+	void Spawn(){
+			int random = Random.Range(0, 3);
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.AddComponent<Rigidbody>();
+			cube.GetComponent<Rigidbody>().useGravity = false;
+			//cube.GetComponent<Collider>().isTrigger = true;
+			switch(random){
+				case 0:
+					cube.gameObject.transform.localPosition = pos1;
+					cube.transform.SetParent(parent.transform, true);
+					break;
+				case 1:
+					cube.gameObject.transform.localPosition = pos2;
+					cube.transform.SetParent(parent.transform, true);
+					break;
+				case 2:
+					cube.gameObject.transform.localPosition = pos3;
+					cube.transform.SetParent(parent.transform, true);
+					break;
+				default: 
+					break;
+			}
+	}
+    void OnCollisionEnter(Collision other) {
+        Destroy(other.gameObject);
+    }
+}
