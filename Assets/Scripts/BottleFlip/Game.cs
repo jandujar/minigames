a0979810c31f0ba0e81ae6010d1e096f6814e201
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-03-13 15:31:04 +0100

    fix

--- Assets/Scripts/BottleFlip/Game.cs
+++ Assets/Scripts/BottleFlip/Game.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game : IMiniGame {
     public enum GameState
@@ -10,46 +11,124 @@ public class Game : IMiniGame {
     }
 
     public GameState state = GameState.Countdown;
-
-    public float max_power = 100f;
+    public float maxPower = 300f;
+    public float minPower = 200f;
     public float power = 0f;
-    private Vector3 initial_pos, inertia;
-
+    [SerializeField]
+    private GameObject canvasSlider;
+    [SerializeField]
+    private GameManager gm;
+    public bool isLaunched = false;
+    [SerializeField] private Text txt;
     [SerializeField]
     private Rigidbody bottle;
 
     [SerializeField]
-    private bool is_flip = false;
+    private float time;
+    public AudioClip Win, Lose;
+    private AudioSource source;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+
+        source = GetComponent<AudioSource>();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (inertia == Vector3.zero) //Si ha parat de moure's
-        {
-            //comprovar rotació i comprovar si l'hem llançat
-            //si la rotació és ~(0,0,0) i comprovar si la rotacio en l'eix Z es < epsilon
-            //float.Epsilon
-        }
+        
 	}
+
+    IEnumerator CheckEnd()
+    {
+        yield return new WaitForSeconds(0.5f);
+        while (true)
+        {
+            if (isLaunched == true)
+            {  //si s'ha tirat l'ampolla 
+               //Debug.Log("velocity: " + bottle.velocity.magnitude);
+               //Debug.Log("rotation: " + bottle.rotation.eulerAngles.z);
+                if (bottle.position.y < 0f && bottle.velocity.magnitude <= Vector3.kEpsilon)//Si ha parat de moure's
+                {
+                    if (Mathf.Abs(bottle.rotation.eulerAngles.z) <= 15f)
+                    {
+                        StartCoroutine(EndWin());
+                        source.PlayOneShot(Win, 1f);
+
+                    }
+                    else
+                    {
+                        StartCoroutine(EndLose());
+                        source.PlayOneShot(Lose, 1f);
+
+                    }
+                }
+            }
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    IEnumerator EndWin()
+    {
+        yield return new WaitForSeconds(1f);
+        gm.EndGame(IMiniGame.MiniGameResult.WIN);
+    }
+    IEnumerator EndLose()
+    {
+        yield return new WaitForSeconds(1f);
+        gm.EndGame(IMiniGame.MiniGameResult.LOSE);
+    }
+
 
     public void Launch()
     {
-        bottle.AddForce(Vector3.up * power);
-        bottle.AddRelativeTorque(Vector3.forward * power);
-        power = 0f;
+        if (!isLaunched)
+        {
+            //Debug.Log("Power: " + power);
+            power = Mathf.Max(minPower, power);
+            bottle.AddForce(Vector3.up * power);
+            bottle.AddRelativeTorque(Vector3.forward * power);
+            power = 0f;
+            isLaunched = true;
+            StartCoroutine(CheckEnd());
+        }
     }
 
     public override void initGame(MiniGameDificulty difficulty, GameManager gm)
     {
-        initial_pos = bottle.transform.position;
         state = Game.GameState.Countdown;
+        canvasSlider.SetActive(false);
+
     }
 
     public override void beginGame()
     {
         state = Game.GameState.Playing;
+        canvasSlider.SetActive(true);
+        isLaunched = false;
+        StartCoroutine(CheckTimeout());
+    }
+
+    IEnumerator CheckTimeout()
+    {
+        txt.text = (time).ToString();
+        for (int i = 0; i < time; i++)
+        {
+            txt.text = (time - i).ToString();
+            yield return new WaitForSeconds(1f);
+        }
+        txt.text = (0).ToString();
+
+        if (!isLaunched && power <= 0)
+        {
+            gm.EndGame(IMiniGame.MiniGameResult.LOSE);
+            source.PlayOneShot(Lose, 1f);
+
+        }
+    }
+    public override string ToString()
+    {
+        return "BottleFlip by Marc";
     }
 }
