095c0678a921c9032903fc406d702f6c3e17785a
joey013 <dracojoy@gmail.com>
2018-03-13 18:00:16 +0100

    DestroyTheWorld Started

---
+++ Assets/Scenes/EndTheWorld/ReadMe.md
@@ -0,0 +1,3 @@
+End The World
+Created by Joel Reverté Naharro (dracojoy@gmail.com)
+2018/02/12
---
+++ Assets/Scripts/EndTheWorld/EndTheWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTheWorld : IMiniGame {
+
+    private GameManager gameManager;
+    public Transform canvas;
+
+    public override void beginGame()
+    {
+        Debug.Log(this.ToString() + " game Begin");
+        canvas.gameObject.SetActive(true);
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+    }
+
+    public override string ToString()
+    {
+        return "End The World by DarkJoe";
+    }
+}
