4e98599b2096fa6d7ec9a27733365030489068fa
Raul Reche <raulreche9@gmail.com>
2018-03-13 16:31:51 +0100

    Crete basics

---
+++ Assets/Scripts/RopePull/MechanicsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MechanicsManager : MonoBehaviour {
+
+    [Header("Animations")]
+    //public Animator animation_name;
+   
+
+    [Header("CanvasText")]
+    //public GameObject text_name;
+  
+    [Header("Classes")]
+    //public VeganKnows Class_name;
+
+    [Header("Variables")]
+    private int m_GameState;
+    public int timeleft = 10;
+
+
+
+    private void Update()
+    {
+        if (m_GameState == 0)
+        {
+        }
+            while (timeleft > -1)
+            {
+
+            }
+            Debug.Log("LOSER");
+        }
+    }
