8ccdd40dfef167cb56e97287bc93a229e443b260
BALMES\marc.alfonso <marc.alfonso@balmes.epiaedu.cat>
2018-03-13 17:23:21 +0100

    primer commit del juego 3d

---
+++ Assets/Scripts/BallRun/BallRun.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallRun : IMiniGame
+{
+    public enum GameState
+    {
+        Countdown,
+        Playing
+    }
+    public GameManager gm;
+
+
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+
+    }
+
+
+
+    /*IEnumerator CheckTimeout()
+    {
+
+    }*/
+    IEnumerator CheckEnd()
+    {
+        yield return new WaitForSeconds(0.5f);
+    }
+    IEnumerator EndWin()
+    {
+        yield return new WaitForSeconds(2f);
+        gm.EndGame(IMiniGame.MiniGameResult.WIN);
+    }
+    IEnumerator EndLose()
+    {
+        yield return new WaitForSeconds(0.2f);
+        gm.EndGame(MiniGameResult.LOSE);
+    }
+    IEnumerator EndLoseLong()
+    {
+        yield return new WaitForSeconds(1f);
+        gm.EndGame(MiniGameResult.LOSE);
+    }
+
+
+
+    public override void beginGame()
+    {
+
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+
+    }
+
+    public override string ToString()
+    {
+        return "BallRun by Marc Alfonso";
+    }
+}
---
+++ Assets/Scripts/BallRun/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10f;
+    public float jumpForce = 30f;
+    public GameObject player;
+    private bool hasJumped = false;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SimpleMovement();
+	}
+    void SimpleMovement()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey(KeyCode.S)) 
+        {
+            player.transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        else if(Input.GetKey(KeyCode.D)) 
+        {
+            player.transform.Translate(Vector3.right* Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.Space) && hasJumped == false)
+        {
+            //player.transform.Translate(Vector3.up * Time.deltaTime * jumpForce);
+            rb.AddForce(Vector3.up * jumpForce);
+            hasJumped = true;
+        }
+    }
+}
