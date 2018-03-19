c1a54c592382d731f1866d3591ce2960ac260303
BALMES\carlos.sevilla <carlos.sevilla@balmes.epiaedu.cat>
2018-03-19 19:35:36 +0100

    Shoot script + 1/2 rotation Shoot

---
+++ Assets/Scripts/Bowling/Rotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    public GameObject Ball;
+    public float speedShoot = 10.0F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        /*transform.rotate(Vector3.left * speedShoot);
+
+        if(ball.rotate.y == -70)
+        {
+            transform.rotate(Vector3.right * speedShoot);
+        }
+        else if (ball.rotate.y == 70)
+        {
+            transform.rotate(Vector3.left * speedShoot);
+        }*/
+
+        transform.Rotate(Vector3.up, speedShoot * Time.deltaTime);
+
+        
+       
+
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/Bowling/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+
+    public float shootForce = 1000;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(shootForce * Vector3.forward);
+    }
+}
