655f8ec2f56a28f4e7a5c483833486f3ce296aab
AnarKi <randomless81@gmail.com>
2018-03-20 18:47:50 +0100

    bomb final?

---
+++ Assets/Scripts/Bomb/bombAudioExplosion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombAudioExplosion : MonoBehaviour {
+
+
+    public BombExitpath bomb;
+
+
+    private void Update()
+    {
+        if (bomb.lose)
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
