3effbb4c35ee836639f0cad631eaf9ac28f8f894
MSI\lordi <rogerk.1996@gmail.com>
2018-03-26 10:03:17 +0100

    cannon game started

---
+++ Assets/Scripts/Cannon/Cannon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : IMiniGame
+{
+	public GameObject[] dianas;//array de objetos
+	public Cannon cannon;
+	private GameManager gameManager;
+	private int score;
+	public int targetScore;
+	bool toLose;
+
+	IEnumerator Win (){
+		yield return new WaitForSeconds (2f);
+		gameManager.EndGame (IMiniGame.MiniGameResult.WIN);
+	}
+
+	IEnumerator Lose(){
+		yield return new WaitForSeconds (2f);
+		gameManager.EndGame (IMiniGame.MiniGameResult.LOSE);	
+	}
+
+	IEnumerator BooleanToTrue(){
+		yield return new WaitForSeconds (10f);
+		toLose = true;
+	}
+
+
+	void Update(){
+		if (score == targetScore) {
+			StartCoroutine (Win ());
+		} else if(toLose) {
+			StartCoroutine (Lose ());
+		}
+	}
+
+
+
+	public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+	{
+		gameManager = gm;
+	}
+
+	public override void beginGame()
+	{
+
+	}
+
+    public override string ToString()
+    {
+        return "Cannon by Roger";
+    }
+}
