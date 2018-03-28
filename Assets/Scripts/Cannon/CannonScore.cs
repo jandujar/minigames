8fc2caadd3338e2e42bd3ef77659311c3ceb9b4c
MSI\lordi <rogerk.1996@gmail.com>
2018-03-28 12:00:38 +0100

    targets spawn done

---
+++ Assets/Scripts/Cannon/CannonScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CannonScore : MonoBehaviour {
+
+	public int score;
+	public Text scoreText; 
+
+	void Update(){
+		scoreText.text = score + "/20";
+		if (score > 20) {
+			score = 20;
+		
+		}
+		if (score < 0) {
+			score = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Cannon/TargetTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetTrigger : MonoBehaviour {
+
+
+	public bool interactable;
+	public bool skullTarget;
+	public GameObject scoreManager;
+
+	void Start(){
+		interactable = true;
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Player" && Input.GetButton ("Fire1") && interactable && skullTarget == false) {
+			interactable = false;
+			scoreManager.GetComponent<CannonScore> ().score += 5;
+			Debug.Log ("colision pal jandu");
+			GetComponent<SpriteRenderer> ().enabled = false;
+		} else if (other.tag == "Player" && Input.GetButton ("Fire1") && interactable && skullTarget == true) {
+			interactable = false;
+			scoreManager.GetComponent<CannonScore> ().score -= 3;
+			Debug.Log ("colision pal jandu");
+			GetComponent<SpriteRenderer> ().enabled = false;	
+		}
+	}
+
+
+
+
+}
