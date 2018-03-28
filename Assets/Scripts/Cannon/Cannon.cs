60f6d89d5afefe779f6d609f3cc47cbae170c54a
MSI\lordi <rogerk.1996@gmail.com>
2018-03-28 12:43:50 +0100

    win and lose fixed

--- Assets/Scripts/Cannon/Cannon.cs
+++ Assets/Scripts/Cannon/Cannon.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class Cannon : IMiniGame
 {
-	public GameObject[] dianas;//array de objetos
 	public Cannon cannon;
 	private GameManager gameManager;
 	private int score;
 	public int targetScore;
+	public GameObject scoreManager;
 	bool toLose;
 
-	IEnumerator Win (){
+	public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+	{
+		this.gameManager = gm;
+	}
+
+	IEnumerator Win(){
 		yield return new WaitForSeconds (2f);
-		gameManager.EndGame (IMiniGame.MiniGameResult.WIN);
+		gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
 	}
 
 	IEnumerator Lose(){
 		yield return new WaitForSeconds (2f);
-		gameManager.EndGame (IMiniGame.MiniGameResult.LOSE);	
+		gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);	
 	}
 
 	IEnumerator BooleanToTrue(){
@@ -28,23 +33,28 @@ public class Cannon : IMiniGame
 
 
 	void Update(){
-		if (score == targetScore) {
-			StartCoroutine (Win ());
-		} else if(toLose) {
-			StartCoroutine (Lose ());
+		Debug.Log (score);
+		score = scoreManager.GetComponent<CannonScore>().score;
+		if (score == 20) {
+			StartCoroutine(Win());
+		} 
+
+		if(toLose) {
+			StartCoroutine(Lose());
 		}
 	}
 
 
 
-	public override void initGame(MiniGameDificulty difficulty, GameManager gm)
-	{
-		gameManager = gm;
-	}
 
+	public void Start(){
+	
+	StartCoroutine(BooleanToTrue());
+	
+	}
 	public override void beginGame()
 	{
-
+		//StartCoroutine(BooleanToTrue());
 	}
 
     public override string ToString()
