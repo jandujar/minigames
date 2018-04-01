dc156f12119857f7005304901bada534d9802f67
CallMeAnarKi <randomless81@gmail.com>
2018-04-01 17:47:11 +0200

    topos colocados

---
+++ Assets/Scripts/Whacamole/whacamoleMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whakamoleMove : MonoBehaviour {
+
+    //PUBLIC
+    public int speed;
+
+    //PRIVATE 
+
+    private float initialPositionX;
+    private float initialPositionY;
+    private float initialPositionZ;
+
+
+
+	// Use this for initialization
+	void Start () {
+        initialPositionX = transform.position.x;
+        initialPositionY = transform.position.y;
+        initialPositionZ = transform.position.z;
+
+	}
+	
+    void Update()
+    {
+
+    }
+
+}
