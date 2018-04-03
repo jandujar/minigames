2c26ebe435ee20604548d02b083960f18bea4f4a
Cano1 <ramon.cano@balmes.epiaedu.cat>
2018-04-03 11:42:48 +0200

    Se dispara a la diana

---
+++ Assets/Scripts/Arco/ArcoArrow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcoArrow : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<Rigidbody>().useGravity = false;
+        transform.eulerAngles = transform.eulerAngles - new Vector3(30, 0, 0);
+    }
+
+
+}
