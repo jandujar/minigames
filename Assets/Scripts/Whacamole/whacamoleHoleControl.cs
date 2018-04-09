53c747e2ea4f2eac1290f9cfe6c47f4e056447cc
CallMeAnarKi <randomless81@gmail.com>
2018-04-09 13:04:36 +0200

    intentar lanzar la corutina de los topos desde el control de agujeros.

--- Assets/Scripts/Whacamole/whacamoleHoleControl.cs
+++ Assets/Scripts/Whacamole/whacamoleHoleControl.cs
@@ -6,93 +6,103 @@ public class whacamoleHoleControl : MonoBehaviour {
 
     public GameObject[] holeControl;
 
+    public whacamoleMove moleMoveControl;
+
     private int randomHole;
     private int randomMole;
 
-    private void Start()
+    private void Awake()
     {
         for (int i = 0; i < 6; ++i)
         {
-            holeControl[i].transform.GetChild(0).gameObject.SetActive(false);
-            holeControl[i].transform.GetChild(1).gameObject.SetActive(false);
+           holeControl[i].transform.GetChild(0).gameObject.SetActive(false);
+           holeControl[i].transform.GetChild(1).gameObject.SetActive(false);
         }
     }
 
-    private void Update()
+    private void Start()
     {
-        randomHole = Random.Range(0, 6);
-        Debug.Log(randomHole);
-        switch (randomHole)
+        StartCoroutine(MyUpdate());
+    }
+
+    private IEnumerator MyUpdate()
+    {
+        while (true)
         {
-            case 0:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[0].transform.GetChild(0).gameObject.SetActive(true);
-                }else
-                {
-                    holeControl[0].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
-            case 1:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[1].transform.GetChild(0).gameObject.SetActive(true);
-                }
-                else
-                {
-                    holeControl[1].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
-            case 2:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[2].transform.GetChild(0).gameObject.SetActive(true);
-                }
-                else
-                {
-                    holeControl[2].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
-            case 3:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[3].transform.GetChild(0).gameObject.SetActive(true);
-                }
-                else
-                {
-                    holeControl[3].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
-            case 4:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[4].transform.GetChild(0).gameObject.SetActive(true);
-                }
-                else
-                {
-                    holeControl[4].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
-            case 5:
-                randomMole = Random.Range(0, 2);
-                if (randomMole == 0)
-                {
-                    holeControl[5].transform.GetChild(0).gameObject.SetActive(true);
-                }
-                else
-                {
-                    holeControl[5].transform.GetChild(1).gameObject.SetActive(true);
-                }
-                break;
+            randomHole = Random.Range(0, 1);
+            switch (randomHole)
+            {
+                case 0:
+                    randomMole = Random.Range(0, 1);
+                    if (randomMole == 0)
+                    {
+                        Debug.Log("entra");
+                        holeControl[0].transform.GetChild(0).gameObject.SetActive(true);
+                        holeControl[0].transform.GetChild(0).gameObject.GetComponent<whacamoleMove>().StartCoroutine(holeControl[0].transform.GetChild(0).gameObject.GetComponent<whacamoleMove>().moveCorutine);
+                    }
+                    else
+                    {
+                        holeControl[0].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+                case 1:
+                    randomMole = Random.Range(0, 2);
+                    if (randomMole == 0)
+                    {
+                        holeControl[1].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        holeControl[1].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+                case 2:
+                    randomMole = Random.Range(0, 2);
+                    if (randomMole == 0)
+                    {
+                        holeControl[2].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        holeControl[2].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+                case 3:
+                    randomMole = Random.Range(0, 2);
+                    if (randomMole == 0)
+                    {
+                        holeControl[3].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        holeControl[3].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+                case 4:
+                    randomMole = Random.Range(0, 2);
+                    if (randomMole == 0)
+                    {
+                        holeControl[4].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        holeControl[4].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+                case 5:
+                    randomMole = Random.Range(0, 2);
+                    if (randomMole == 0)
+                    {
+                        holeControl[5].transform.GetChild(0).gameObject.SetActive(true);
+                    }
+                    else
+                    {
+                        holeControl[5].transform.GetChild(1).gameObject.SetActive(true);
+                    }
+                    break;
+            }
+            yield return new WaitForSeconds(3);
         }
-
-
-
     }
 
 
--- Assets/Scripts/Whacamole/whacamoleMove.cs
+++ Assets/Scripts/Whacamole/whacamoleMove.cs
@@ -2,8 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class whakamoleMove : MonoBehaviour {
+public class whacamoleMove : MonoBehaviour {
 
+    public IEnumerator moveCorutine;
+    public bool active;
+
+    private void Start() {
+        moveCorutine = move();
+    }
+
+
+
+    IEnumerator move()
+    {
+        active = true;
+        Debug.Log("active_true");
+        yield return new WaitForSeconds(1.2f);
+        Debug.Log("yield");
+        active = false;
+        Debug.Log("active_false");
+        StopCoroutine(moveCorutine);
+        Debug.Log("stop_corutine");
+        this.gameObject.SetActive(false);
+        Debug.Log("desactivar");
+    }
 
 
 }
