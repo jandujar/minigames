b1bd14747a4de97d678872d5ceeebe4f33919d12
AnarKi <randomless81@gmail.com>
2018-04-10 20:09:02 +0200

    martillo se mueve

---
+++ Assets/Scripts/Whacamole/WhacamoleHammer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhacamoleHammer : MonoBehaviour {
+
+    private float moveX;
+    private float moveY;
+    private float moveZ;
+    private float incrementX;
+    private float incrementY;
+    private float speed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+        moveX = transform.position.x;
+        moveY = transform.position.y;
+        moveZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        incrementX = InputManager.Instance.GetAxisHorizontal()*3;
+        incrementY = InputManager.Instance.GetAxisVertical()*3;
+        transform.Translate(incrementX * speed * Time.deltaTime, 0, incrementY * speed * Time.deltaTime);
+    }
+}
