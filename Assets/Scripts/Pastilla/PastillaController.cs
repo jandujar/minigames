8737a10e9a9a0bb917ff30154e3afcbfafb8e40a
carles.condeminas <carles.condeminas@gmail.com>
2018-04-10 16:50:48 +0200

    Pastilla V2

---
+++ Assets/Scripts/Pastilla/PastillaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PastillaController : MonoBehaviour {
+    public float speed;
+
+    private Rigidbody rb;
+
+    public float jumpForce = 2000f;	// Variable that determines the forward force
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+        if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+        }
+    }
+}
