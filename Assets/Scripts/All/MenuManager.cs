5dbba2407d20ffabe3ac1cc8e87c3df67dcd790e
Jose Antonio Andujar <jandujar@jandusoft.com>
2018-04-17 19:01:15 +0200

    find treasure

--- Assets/Scripts/All/MenuManager.cs
+++ Assets/Scripts/All/MenuManager.cs
@@ -46,6 +46,7 @@ public class MenuManager : Singleton<MenuManager> {
         BMX,
         DESTROY,
         BALLRUN,
+        TREASURE,
 		END };
 	
     private MINIGAMES_ENUM currentGame = MINIGAMES_ENUM.PONG;
@@ -174,6 +175,9 @@ public class MenuManager : Singleton<MenuManager> {
             break;
         case MINIGAMES_ENUM.BALLRUN:
             SceneManager.LoadScene("BallRun");
+            break;
+        case MINIGAMES_ENUM.TREASURE:
+            SceneManager.LoadScene("FindTreasure");
             break;
         case MINIGAMES_ENUM.END:
 		default:
