e06c5d2347d50b064297297e3abc11f71873da8b
Dinith <carles.condeminas@gmail.com>
2018-05-22 10:19:36 +0200

    Lava added

--- Assets/Scripts/Pastilla/PacmanController.cs
+++ Assets/Scripts/Pastilla/PacmanController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PacmanController : MonoBehaviour {
     public Transform target;
     public float speed;
+    [SerializeField]
+    private GameManager gameManager;
 
     void Update()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {Debug.Log("lost");
+        if (collision.gameObject.name == "Final")
+        {            
+            gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+        }
     }
 }
