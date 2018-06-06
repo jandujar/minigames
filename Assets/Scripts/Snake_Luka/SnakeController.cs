b06f3a5b130699c0859d073e3fca15b2ebcad535
Luka Giacco <devourerofthecosmos@hotmail.com>
2018-06-06 18:41:13 +0200

    Proyecto empezado

---
+++ Assets/Scripts/Snake_Luka/SnakeController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+
+    public GameManager gameManager = null;
+    public Snake_Luka game = null;
+
+    [SerializeField]
+    GameObject[] body;
+
+    [SerializeField]
+    float speed = 0f;
+
+    Vector3 initPos,
+        finalPos;
+
+    float movementDistance = 1f;
+
+    enum Orders
+    {
+        FORWARD,
+        LEFT,
+        RIGHT
+    }
+
+    Orders lastOrder = Orders.FORWARD;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "schutzstaffel_b") //monedita
+        {
+            Destroy(collision.gameObject);
+            game.Score++;
+
+            if (game.Score >= game.TargetScore)
+            {
+                gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
+            }
+        }
+        else if (collision.collider.tag == "schutzstaffel_a" || collision.collider.tag == "wehrmacht_b")
+        {
+            gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+        }
+    }
+
+    public void StartMoving()
+    {
+        StartCoroutine(Move(lastOrder));
+    }
+
+    IEnumerator Move(Orders direction)
+    {
+        initPos = transform.position;
+        Vector3 directionVector = Vector3.zero;
+        float angle = 0f;
+        switch (direction)
+        {
+            case Orders.FORWARD:
+                angle = 0f;
+                directionVector = Vector3.up * movementDistance;
+                break;
+            case Orders.LEFT:
+                angle = -90f;
+                directionVector = Vector3.left * movementDistance;
+                break;
+            case Orders.RIGHT:
+                angle = 90f;
+                directionVector = Vector3.right * movementDistance;
+                break;
+        }
+        finalPos = initPos + directionVector;
+
+        transform.Rotate(Vector3.forward, angle);
+
+        body[0].transform.LookAt(transform);
+
+        for (int i = 1; i < body.Length; i++)
+        {
+            body[i].transform.LookAt(body[i-1].transform);
+        }
+
+        while ((finalPos - transform.position).magnitude >= Vector3.kEpsilon)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+            for (int i = 0; i < body.Length; i++)
+            {
+                body[i].transform.Translate(Vector3.up * Time.deltaTime * speed);
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        //Clavar la posicio de les peces
+
+        StartCoroutine(Move(lastOrder));
+    }
+}
---
+++ Assets/Scripts/Snake_Luka/Snake_Luka.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake_Luka : IMiniGame
+{
+
+    [SerializeField]
+    int score = 0,
+        targetScore = 20;
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public int TargetScore
+    {
+        get { return targetScore; }
+    }
+
+    [SerializeField]
+    float time = 30f,
+            currentTime = 0f;
+
+    [SerializeField]
+    SnakeController snake = null;
+
+    GameManager gameManager = null;
+
+    void Awake()
+    {
+        //Init Pong
+        Debug.LogError("Change this Script for your own Script");
+    }
+
+    public override void beginGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        gameManager = gm;
+        snake.gameManager = gm;
+        snake.game = this;
+    }
+    	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override string ToString()
+    {
+        return "Snake by Luka";
+    }
+}
