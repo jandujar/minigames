27369daea32197edda94ea4961bde56dfa6a6c77
Raul Reche <raulreche9@gmail.com>
2018-06-12 22:54:35 +0100

    Camera changes

--- Assets/Scripts/Snake_raul/Snake.cs
+++ Assets/Scripts/Snake_raul/Snake.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Snake : MonoBehaviour {
 
     public GameObject camera;
+    private Vector3 offset;
     float xvel, zvel;
-    enum Direcciones { delante, izquierda, derecha, detras };
-    private Direcciones direcciones;
+   // enum Direcciones { delante, izquierda, derecha, detras };
+    //private Direcciones direcciones;
+    private Vector3 Rotation;
+
 
     // Use this for initialization
     void Start () {
-        float zvel = 0f;
-        float xvel = 0.02f;
-        direcciones = Direcciones.delante;
-	}
+        zvel = 0f;
+        xvel = 0.02f;
+        offset = new Vector3(0,1.2f,-4);
+        //direcciones = Direcciones.delante;
+    }
 	
-	// Update is called once per frame
 	void Update () {
 
         transform.Translate(zvel, 0f, xvel);
@@ -24,41 +27,52 @@ public class Snake : MonoBehaviour {
             if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
             {
                 Debug.Log("izq");
-                switch (direcciones)
-                {
-                    case Direcciones.delante:
-                        xvel = 0;
-                        zvel = -0.02F;
-                        direcciones = Direcciones.izquierda;
-                        break;
-                    case Direcciones.izquierda:
-                        xvel = -0.02F;
-                        zvel = 0;
-                        direcciones = Direcciones.detras;
-                        break;
-                    case Direcciones.detras:
-                        xvel = 0;
-                        zvel = 0.02F;
-                        direcciones = Direcciones.derecha;
-                        break;
-                    case Direcciones.derecha:
-                        xvel = 0.02F;
-                        zvel = 0;
-                        direcciones = Direcciones.delante;
-                        break;
-                    default:
-                        break;
-                }
-            }
+                xvel = 0.02F;
+                zvel = 0;
+                Rotation = new Vector3(0, -90, 0);
+                offset = new Vector3(0, 1.2f, 4);
+                transform.Rotate(Rotation);
+            /* switch (direcciones)
+             {
+                 case Direcciones.delante:
+                     xvel = 0;
+                     zvel = -0.02F;
+                     direcciones = Direcciones.izquierda;
+                     break;
+                 case Direcciones.izquierda:
+                     xvel = -0.02F;
+                     zvel = 0;
+                     direcciones = Direcciones.detras;
+                     break;
+                 case Direcciones.detras:
+                     xvel = 0;
+                     zvel = 0.02F;
+                     direcciones = Direcciones.derecha;
+                     break;
+                 case Direcciones.derecha:
+                     xvel = 0.02F;
+                     zvel = 0;
+                     direcciones = Direcciones.delante;
+                     break;
+                 default:
+                     break;
+             }*/
+        }
 
             if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2))
             {
                 Debug.Log("der");
-            switch (direcciones)
+            xvel = 0.02F;
+            zvel = 0;
+            Rotation = new Vector3(0, 90, 0);
+            transform.Rotate(Rotation);
+            /*switch (direcciones)
                 {
                     case Direcciones.delante:
-                        xvel = 0;
-                        zvel = 0.02F;
+                        xvel = 0.02F;
+                        zvel = 0;
+                        Rotation = new Vector3(0, 90, 0);
+                        transform.Rotate(Rotation);
                         direcciones = Direcciones.derecha;
                         break;
                     case Direcciones.izquierda:
@@ -78,9 +92,14 @@ public class Snake : MonoBehaviour {
                         break;
                     default:
                         break;
-                }
-            }
+                }*/
         }
+    }
+
+    void LateUpdate()
+    {
+        camera.transform.position = this.transform.position + offset;
+    }
 
 }
 
