53ed03a58ca312d37cce234ccc90b0e79be25508
Raul Reche <raulreche9@gmail.com>
2018-06-15 00:21:00 +0100

    Acabados creditos

---
+++ Assets/Scripts/Creditos_raul/Credits_raul.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits_raul : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene("Menu_raul");
+        }
+	}
+}
