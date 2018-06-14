83bf040f65d2f57c5bef8ea564d2edea98cc14db
Home_PC <marc.escobar1@gmail.com>
2018-06-14 19:18:15 +0200

    Menus

---
+++ Assets/Scripts/Menus_MarcEscobar/Credits_MarcEscobar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits_MarcEscobar : MonoBehaviour {
+	public GameObject Text;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text.transform.Translate (Text.transform.up * speed * Time.deltaTime);
+		if (Input.anyKeyDown) {
+			BackToMenu ();
+		}
+	}
+
+	void BackToMenu(){
+		//CARGAR EL MENU DE NUEVO
+		Debug.Log("Skip");
+	}
+}
---
+++ Assets/Scripts/Menus_MarcEscobar/Menu_MarcEscobar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_MarcEscobar : MonoBehaviour {
+
+
+	public void Play(){
+		//CARGAR JUEGOS
+	}
+	public void Credits(){
+		//CARGAR ESCENA CREDITOS
+	}
+	public void Exit(){
+		Application.Quit ();
+	}
+}
