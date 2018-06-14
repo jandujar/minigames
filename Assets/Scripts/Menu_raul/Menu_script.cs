602166018004ff6c7c7b237aac2aeabd86f67c5f
Raul Reche <raulreche9@gmail.com>
2018-06-14 23:51:55 +0100

    Menu commit

---
+++ Assets/Scripts/Menu_raul/Menu_script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_script : MonoBehaviour {
+
+    public Button Jugar;
+    public Button Creditos;
+    public Button Salir;
+    public Text Credits;
+    public Image fondo;
+    private bool cred;
+
+    // Use this for initialization
+    void Start () {
+        Button creditos = Creditos.GetComponent<Button>(); 
+        creditos.onClick.AddListener(CreditsClick);
+
+        Button salir = Salir.GetComponent<Button>();
+        salir.onClick.AddListener(SalirClick);
+
+        cred = true;
+    }
+	
+    public void CreditsClick()
+    {
+        if (cred == true) {
+            Credits.gameObject.SetActive(true);
+            fondo.gameObject.SetActive(false);
+            cred = false;
+        }
+        else if (cred == false)
+        {
+            Credits.gameObject.SetActive(false);
+            fondo.gameObject.SetActive(true);
+        }
+
+    }
+
+    public void SalirClick()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
