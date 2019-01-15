073a6ab718e24c7b0032077bb3ebf241107569b8
Vanik <ivan.alvarez.enri.oficial@gmail.com>
2019-01-15 18:28:11 +0100

    Movimiento Mano Borracha

--- Assets/Scripts/Drunkey/Drunkey_Sc.cs
+++ Assets/Scripts/Drunkey/Drunkey_Sc.cs
@@ -6,16 +6,65 @@ namespace ivan_alvarez_enri
     public class Drunkey_Sc : IMiniGame
     {
         private GameManager gameManager;
-        // Start is called before the first frame update
-        public override void beginGame()
-        {
-            //throw new System.NotImplementedException();
-        }
+        public Animator buttons;
+        public GameObject Player;
+        private bool StartInputs = false;
+        private bool Begin = false;
+        private float xDir= 1;
+        private float yDir= 1;
+
         public override void initGame(MiniGameDificulty difficulty, GameManager gm)
         {
+            
             this.gameManager = gm;
+            Player = GameObject.FindGameObjectWithTag("Player");
             //throw new System.NotImplementedException();
         }
+
+        public override void beginGame()
+        {
+            Begin = true;
+            StartCoroutine(changeX(2));
+            StartCoroutine(changeY(1));
+            //throw new System.NotImplementedException();
+        }
+        private void Update()
+        {
+        if (Begin) { 
+            if (!StartInputs)
+            {
+                InitAnimation();
+            }
+            else
+            {
+
+            }
+                Player.transform.Translate(new Vector3(0.02F*xDir,0.02F*yDir, 0));
+            }
+        }
+        public void InitAnimation() { 
+           
+            if (Player.transform.position.y<-27F){
+                Player.transform.Translate(new Vector3(0, 0.2F, 0));
+            }
+            else
+            {
+                StartInputs = true;
+            }
+        }
+        IEnumerator changeX(float time)
+        {
+            yield return new WaitForSeconds(time);
+            xDir *= -1;
+            StartCoroutine(changeX(6));
+        }
+        IEnumerator changeY(float time)
+        {
+            yield return new WaitForSeconds(time);
+            yDir *= -1;
+            StartCoroutine(changeY(3));
+        }
+
 
     }
 }
