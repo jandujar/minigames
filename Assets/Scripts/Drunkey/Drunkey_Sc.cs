0394de0e2c526762b0d86c29ea441524021065ef
Vanik <ivan.alvarez.enri.oficial@gmail.com>
2019-01-22 17:13:29 +0100

    Condicion de derrota

--- Assets/Scripts/Drunkey/Drunkey_Sc.cs
+++ Assets/Scripts/Drunkey/Drunkey_Sc.cs
@@ -7,11 +7,22 @@ namespace ivan_alvarez_enri
     {
         private GameManager gameManager;
         public Animator buttons;
-        public GameObject Player;
-        private bool StartInputs = false;
-        private bool Begin = false;
-        private float xDir= 1;
-        private float yDir= 1;
+        private GameObject Player;
+        public bool StartInputs = false;
+        public bool Begin = false;
+        public bool lose=false;
+        public bool win=false;
+        public float xDir= 1;
+        public float yDir= 1;
+        Buttons CorrectBtn;
+
+        IEnumerator currentCoroutineX, currentCoroutineY;
+
+        private enum Buttons
+        {
+            A,
+            B
+        }
 
         public override void initGame(MiniGameDificulty difficulty, GameManager gm)
         {
@@ -24,23 +35,65 @@ namespace ivan_alvarez_enri
         public override void beginGame()
         {
             Begin = true;
-            StartCoroutine(changeX(2));
-            StartCoroutine(changeY(1));
+            currentCoroutineX = changeX(2);
+            StartCoroutine(currentCoroutineX);
+            currentCoroutineY = changeY(1);
+            StartCoroutine(currentCoroutineY);
             //throw new System.NotImplementedException();
         }
         private void Update()
         {
-        if (Begin) { 
-            if (!StartInputs)
-            {
-                InitAnimation();
+            if(!lose&&!win){
+                if (Begin) { 
+                    if (!StartInputs)
+                    {
+                        InitAnimation();
+                    }
+                    else
+                    {
+                        if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1)){
+                            CorrectBtn=Buttons.B;
+                            buttons.SetBool("ButtonA",false);
+                            buttons.SetBool("ButtonB",true);
+                            Player.transform.Translate(new Vector3(0,1, 0));
+                            Begin=false;
+                        }else if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2)){
+                            CorrectBtn=Buttons.A;
+                            buttons.SetBool("ButtonA",true);
+                            buttons.SetBool("ButtonB",false);
+                            Player.transform.Translate(new Vector3(0,1, 0));
+                            Begin=false;
+                        }
+                    }
+                        
+                    }
+                else{
+                        if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1)&&CorrectBtn==Buttons.A){
+                            CorrectBtn=Buttons.B;
+                            buttons.SetBool("ButtonA",false);
+                            buttons.SetBool("ButtonB",true);
+                            Player.transform.Translate(new Vector3(0,1, 0));
+                            
+                        }else if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2)&&CorrectBtn==Buttons.A){
+                            Player.transform.Translate(4,0,0);
+                        }else if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2)&&CorrectBtn==Buttons.B){
+                            CorrectBtn=Buttons.A;
+                            buttons.SetBool("ButtonA",true);
+                            buttons.SetBool("ButtonB",false);
+                            Player.transform.Translate(new Vector3(0,1, 0));
+                            
+                        }else if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1)&&CorrectBtn==Buttons.B){
+                            Player.transform.Translate(-4,0,0);
+                        }
+                    }
+            }else if(lose){
+                StopCoroutine(currentCoroutineX);
+                StopCoroutine(currentCoroutineY);
+            }else if(win){
+                StopCoroutine(currentCoroutineX);
+                StopCoroutine(currentCoroutineY);
             }
-            else
-            {
-
-            }
-                Player.transform.Translate(new Vector3(0.02F*xDir,0.02F*yDir, 0));
-            }
+            Player.transform.Translate(new Vector3(0.02F*xDir,0.02F*yDir, 0));
         }
         public void InitAnimation() { 
            
@@ -56,13 +109,20 @@ namespace ivan_alvarez_enri
         {
             yield return new WaitForSeconds(time);
             xDir *= -1;
-            StartCoroutine(changeX(6));
+            currentCoroutineX = changeX(6);
+            StartCoroutine(currentCoroutineX);
         }
         IEnumerator changeY(float time)
         {
             yield return new WaitForSeconds(time);
             yDir *= -1;
-            StartCoroutine(changeY(3));
+            if(Begin){
+                currentCoroutineY = changeY(3);
+                StartCoroutine(currentCoroutineY);
+            }
+            else{
+                yDir=-1;
+            }
         }
 
 
---
+++ Assets/Scripts/Drunkey/OnTriggerEnterLose.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnterLose : MonoBehaviour
+{
+    public bool EjeX;
+    public int Dir;
+
+    public ivan_alvarez_enri.Drunkey_Sc GameManager;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entro");
+        if(GameManager.StartInputs)
+            GameManager.lose=true;
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(GameManager.StartInputs){
+            if(EjeX){
+                GameManager.xDir+=Dir*0.25F;
+            }else{
+                GameManager.yDir-=0.25F;
+            }
+        }
+    }
+}
