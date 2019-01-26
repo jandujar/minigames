742c8eb21fc1851c6967bcc9f1a0c0c1d32393a9
Teteju <jmaldonado.gamedesign@gmail.com>
2019-01-26 02:28:19 +0100

    The game of impossible game added

---
+++ Assets/Scripts/ImpossibleGame_JavierMaldonado/ColorGradient.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGradient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+        var uv = mesh.uv;
+        var colors = new Color[uv.Length];
+
+        // Instead if vertex.y we use uv.x
+        for (var i = 0; i < uv.Length; i++)
+            colors[i] = Color.Lerp(Color.red, Color.green, uv[i].x);
+
+        mesh.colors = colors;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
