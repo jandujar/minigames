d293e3dcd8f8218096a726246714e3b38376d1f5
Eric <esanchezverges@gmail.com>
2019-01-28 21:19:15 +0100

    Trillero done

--- Assets/Scripts/Trilero_Eric/GameHandler.cs
+++ Assets/Scripts/Trilero_Eric/GameHandler.cs
@@ -6,35 +6,72 @@ namespace Eric_Sanchez_Verges
 {
     public class GameHandler : IMiniGame
     {
+        GameManager gameManager;
+        public Camera cam;
         public GameObject cupPrefab, ball;
         GameObject[] cups = new GameObject[3];
         public Vector3[] cupsInitPosition = new Vector3[3];
         public Vector3[] ballInitPosition = new Vector3[3];
         int step;
-       
+        public float showVel = 5f;
+        public Vector2[] movements = new Vector2[10];
+        int movementsIterator = 0;
+        bool startMovements = false;
+        bool test = false;
+        bool init;
+
 
         public override void beginGame()
         {
-            throw new System.NotImplementedException();
+            Debug.Log("BeginGame");
+            init = true;
         }
 
         public override void initGame(MiniGameDificulty difficulty, GameManager gm)
         {
-            throw new System.NotImplementedException();
+            Debug.Log("InitGame");
+            gameManager = gm;
+        }
+
+        void UpdateControlls()
+        {
+            if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
+            {
+                EndGame(true);
+            }
+            if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2))
+            {
+                EndGame(false);
+            }
+        }
+
+        public void EndGame(bool win)
+        {
+            if (win)
+            {
+                gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
+            }
+            else
+            {
+                gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+            }
         }
 
         // Start is called before the first frame update
         void Start()
         {
-           
+            startMovements = false;
+
+
             for (int i = 0; i < cups.Length; i++)
             {
                 cups[i] = Instantiate(cupPrefab);
                 cups[i].transform.position = cupsInitPosition[i];
                 cups[i].GetComponent<cupController>().currentPosition = i;
-
             }
-            ball.transform.position = ballInitPosition[UnityEngine.Random.Range(0, 2)];
+            int rand = UnityEngine.Random.Range(0, 2);
+            cups[rand].GetComponent<cupController>().hasBall = true;
+            ball.transform.position = ballInitPosition[rand];
            
         }
 
@@ -43,19 +80,68 @@ namespace Eric_Sanchez_Verges
         // Update is called once per frame
         void Update()
         {
-            if(cups[0].GetComponent<cupController>().endPath && cups[1].GetComponent<cupController>().endPath && cups[2].GetComponent<cupController>().endPath)
+            if(!startMovements)StartCoroutine(showBall());
+            if(!startMovements)StartCoroutine(StartMovements());
+            if (startMovements && !test)
             {
+                ball.SetActive(false);
+                switch (step)
+                {
+                    case 0:
+                        test = shufle(0, 1);
+                        break;
+
+                    case 1:
+                        test = shufle(1, 2);
+                        break;
+
+                    case 2:
+                        test = shufle(0, 1);
+                        break;
+
+                    case 3:
+                        test = shufle(1, 2);
+                        break;
+
+                    case 4:
+                        test = shufle(0, 1);
+                        break;
+                }
                 
+            }
+             
+            if (test)
+            {
+                test = false;
                 step++;
             }
 
-           
-                for (int i = 0; i < cups.Length; i++)
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit))
                 {
-                    if(i == 1)cups[i].GetComponent<cupController>().moveRight();
-                    else cups[i].GetComponent<cupController>().moveLeft();
+                    for (int i = 0; i < cups.Length; i++)
+                    {
+                        if (hit.collider.gameObject == cups[i])
+                        {
+                            if (cups[i].GetComponent<cupController>().hasBall)
+                            {
+                                EndGame(true);
+                            }
+                            else
+                            {
+                                EndGame(false);
+                            }
+                            Debug.Log("Hit on" + hit.collider.gameObject.name);
+                        }
+                    }
+
+                }
             }
-           
+            
         }
 
         private void OnDrawGizmos()
@@ -69,5 +155,46 @@ namespace Eric_Sanchez_Verges
                 Gizmos.DrawSphere(ballInitPosition[i], 0.25f);
             }
         }
+
+        IEnumerator showBall()
+        {
+            yield return new WaitForSeconds(5);
+            for(int i = 0; i < cups.Length; i++)
+            {
+                cups[i].transform.position = Vector3.MoveTowards(cups[i].transform.position, new Vector3(cups[i].transform.position.x, 0.04f, cups[i].transform.position.y), showVel * Time.deltaTime);  
+            }
+        }
+
+        bool shufle(int firstC, int secondC)
+        {
+                cups[firstC].GetComponent<cupController>().moveRight();
+                return cups[secondC].GetComponent<cupController>().moveLeft();
+        }
+        
+        IEnumerator StartMovements()
+        {
+            
+            yield return new WaitForSeconds(6);
+            startMovements = true;
+
+           
+
+        }
+        void makeMovements()
+        {
+            int x = Mathf.RoundToInt(movements[movementsIterator].x);
+            int y = Mathf.RoundToInt(movements[movementsIterator].y);
+            bool pass = shufle(x, y);
+            Debug.Log("True");
+            if (pass && movementsIterator < movements.Length)
+            {
+                movementsIterator++;
+                Debug.Log("True");
+            }
+        }
+
+       
+
+       
     }
 }
