c892294c9555e5792866dd3219baa94431643d3b
Joaquim Tolosa <quimmare@gmail.com>
2019-01-28 17:16:53 +0100

    Doodlejump

--- Assets/Scripts/DoodleJump/DoodleJump.cs
+++ Assets/Scripts/DoodleJump/DoodleJump.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace joaquim_tolosa
 {
     public class DoodleJump : IMiniGame
     {
         public GameManager gm;
-        bool begin = false; 
+        bool begin = false;
+
         // Start is called before the first frame update
         public override void beginGame()
         {
@@ -22,17 +24,30 @@ namespace joaquim_tolosa
 
         void Update()
         {
+            //victoria
+            if (ScoreSr.scoreValue >= 600)
+            {
+                gm.EndGame(IMiniGame.MiniGameResult.WIN);
+            }
             if (begin)
             {
                 if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON4))
                 {
-                    gm.EndGame(IMiniGame.MiniGameResult.WIN);
+                    //gm.EndGame(IMiniGame.MiniGameResult.WIN);
                 }
                 if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
                 {
-                    gm.EndGame(IMiniGame.MiniGameResult.LOSE);
+                    //gm.EndGame(IMiniGame.MiniGameResult.LOSE);
                 }
+                
             }
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            //aqui pongo la derrota
+            gm.EndGame(IMiniGame.MiniGameResult.LOSE);
+            //Debug.Log("hola");
+
         }
     }
 }
---
+++ Assets/Scripts/DoodleJump/ScoreSr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSr : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Score: " + scoreValue;
+    }
+}
