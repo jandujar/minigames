c42a3b6ccb86ab7236a7fd19a8b201fe0a7e2961
MSI\Omega <joan.serrano@balmes.epiaedu.cat>
2019-01-29 15:20:45 +0100

    Revert "Revert "Done""
    This reverts commit 57738ad59984c6a0eb60fd15f65f404d10a3da67.

---
+++ Assets/Scripts/BlindShot/Target.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Canvas gCan;
+
+    public bool stop = false;
+
+    int random = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        random = UnityEngine.Random.Range(4, 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gCan.enabled)
+        {
+            if (InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON4))
+            {
+                stop = true;
+            }
+            if (!stop)
+            {
+                RectTransform myRect = GetComponent<RectTransform>();
+
+                myRect.transform.position = new Vector2(myRect.position.x + random, myRect.position.y);
+            }
+        }
+    }
+    
+}
