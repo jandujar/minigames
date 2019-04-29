d013ec93ac6487f267ee5bc10b10e30374eacadf
mario hernandez <marioherferj@gmail.com>
2019-04-29 16:41:22 +0200

    Create SuperHexagon

---
+++ Assets/Scripts/SuperHexagon/SuperHexagon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperHexagon : IMiniGame
+{
+
+    public TheHexagon theHexagon;
+
+    void Awake()
+    {
+        ////Init BlckJack
+        ////Debug.LogError("Change this Script for your own Script");
+    }
+
+    public override void beginGame()
+    {
+        ////Pong Begins
+        Debug.Log(this.ToString() + " game Begin");
+        GetComponent<AudioSource>().Play();
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        theHexagon.init(gm);
+    }
+
+    public override string ToString()
+    {
+        return "SuperHexagon by Mario_Her/Fer";
+    }
+}
---
+++ Assets/Scripts/SuperHexagon/TheHexagon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheHexagon : MonoBehaviour
+{
+    
+    private GameManager gameManager;
+    //--------------------------------------
+
+
+    public void init(GameManager gm)
+    {
+        gameManager = gm;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // "control!!"
+        if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
+            Win();
+
+        //"alt!!"
+        if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2))
+            Lose();
+    }
+
+
+    private void Lose() { gameManager.EndGame(IMiniGame.MiniGameResult.LOSE); }
+    private void Win() { gameManager.EndGame(IMiniGame.MiniGameResult.WIN); }
+}
