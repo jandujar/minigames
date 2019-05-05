2f21ad1c89d297915670b251103132e0172e948e
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2019-05-05 12:12:04 +0200

    Primera seleccion Hecha

---
+++ Assets/Scripts/AlchemyWars/ChangeSpriteAW.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ivan_alvarez_enri
+{public class ChangeSpriteAW : MonoBehaviour
+{
+    public SpriteRenderer affectChange;
+    public Sprite newSprite;
+   public void Changesprite(){
+       affectChange.sprite=newSprite;
+   }
+}
+}
---
+++ Assets/Scripts/AlchemyWars/SelectableObjectsAW.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ivan_alvarez_enri
+{
+public class SelectableObjectsAW : MonoBehaviour
+    {
+        public Elements _element;
+        public bool selected=false;
+        private Vector3 startPos;
+        private void Start() {
+            startPos=gameObject.transform.position;
+            
+        }
+        public void SelectMe(){
+            gameObject.GetComponent<SpriteRenderer>().color=new Color(1,1,1,1);
+            gameObject.transform.localScale=new Vector3(12,12,12);
+        }
+        public void StopMe(){
+            gameObject.GetComponent<SpriteRenderer>().color=new Color(0.764151F,0.764151F,0.764151F,1);
+            gameObject.transform.localScale=new Vector3(10,10,10);
+           
+        }
+        public void FinishMe(){
+            gameObject.GetComponent<Animator>().SetTrigger("Die");
+        }
+        public void ImChoosed(){
+            gameObject.GetComponent<Animator>().SetBool("Choosed",true);
+        }
+    }
+}
