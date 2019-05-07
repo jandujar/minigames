55ef8b85882ed0365a7cec16e2e3b0a1afba593e
Laura Romo <laura_romo_89@hotmail.es>
2019-05-07 18:09:51 +0200

    Cursor moviendose y con raycast

--- Assets/Scripts/Picross3D/picrossCursor.cs
+++ Assets/Scripts/Picross3D/picrossCursor.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class picrossCursor : MonoBehaviour {
 
-    public Texture2D cursorImage;
     bool left;
     bool right;
+    bool up;
+    bool down;
     bool tope;
     // Start is called before the first frame update
     void Start() {
-        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.Auto);
         bool right = false;
         bool left = false;
         bool tope = false;
@@ -18,24 +18,74 @@ public class picrossCursor : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (InputManager.Instance.GetAxisHorizontal() > 0.1f) {
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit " + hit.collider.gameObject);
+        }
+        else {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+
+        if (InputManager.Instance.GetAxisHorizontal() > 0) {
             if (InputManager.Instance.GetAxisHorizontal() == 1) {
                 left = true;
                 tope = true;
             }
             if (!tope || tope && left) {
-                Debug.Log("Move cursor left");
+                transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
             }
         }
 
-        else if (InputManager.Instance.GetAxisHorizontal() < -0.1f) {
+        else if (InputManager.Instance.GetAxisHorizontal() < 0) {
             if (InputManager.Instance.GetAxisHorizontal() == -1) {
                 right = true;
                 tope = true;
             }
             if (!tope || tope && right) {
+                transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
                 Debug.Log("Move cursor right");
             }
+        }
+
+        if (InputManager.Instance.GetAxisVertical() > 0) {
+            if (InputManager.Instance.GetAxisVertical() == 1) {
+                up = true;
+                tope = true;
+            }
+            if (!tope || tope && up) {
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+            }
+        }
+
+        else if (InputManager.Instance.GetAxisVertical() < 0) {
+            if (InputManager.Instance.GetAxisVertical() == -1) {
+                down = true;
+                tope = true;
+            }
+            if (!tope || tope && down) {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+                Debug.Log("Move cursor right");
+            }
+        }
+
+        if (InputManager.Instance.GetAxisHorizontal() < 1 && left) {
+            left = false;
+        }
+
+        if (InputManager.Instance.GetAxisHorizontal() > -1 && right) {
+            right = false;
+        }
+
+        if (InputManager.Instance.GetAxisVertical() < 1 && up) {
+            up = false;
+        }
+
+        if (InputManager.Instance.GetAxisVertical() > -1 && down) {
+            down = false;
         }
     }
 }
