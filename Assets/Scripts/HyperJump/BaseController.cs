d34c60967173088af9476ffb51a039b16d54498e
Eric Sanchez <esanchezverges@gmail.com>
2019-05-07 16:50:12 +0200

    Movimiento Base

--- Assets/Scripts/HyperJump/BaseController.cs
+++ Assets/Scripts/HyperJump/BaseController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BaseController : MonoBehaviour
+namespace HyperJump
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BaseController : MonoBehaviour
     {
-        
-    }
+        Vector3 rotation;
+        [SerializeField] float rotationSpeed;
+        float axis;
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            axis = Input.GetAxis("Horizontal");
+        }
+
+        void FixedUpdate()
+        {
+            rotation = transform.rotation.eulerAngles;
+            rotation += Vector3.up * axis * rotationSpeed;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
