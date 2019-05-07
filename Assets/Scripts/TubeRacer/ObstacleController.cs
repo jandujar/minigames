ce7d5080c2728dfbb786933e88beed34efd51aaa
Eric Sanchez <esanchezverges@gmail.com>
2019-05-07 16:22:30 +0200

    Cosas

---
+++ Assets/Scripts/TubeRacer/ObstacleController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eric_Sanchez_Verges
+{
+    public class ObstacleController : MonoBehaviour
+    {
+
+        [SerializeField] protected float speed;
+        [SerializeField] protected float forwardSpeed;
+
+        protected float x, y, z;
+        protected float angularSpeed = 0f;
+
+        protected MeshRenderer render;
+        [SerializeField] protected int dir; //-1 = clockwise / 1 = laltre
+                                            // Start is called before the first frame update
+        protected virtual void Start()
+        {
+            render = GetComponent<MeshRenderer>();
+            Initialize();
+        }
+
+        // Update is called once per frame
+        protected virtual void FixedUpdate()
+        {
+            x = Mathf.Cos(dir * angularSpeed);
+            y = Mathf.Sin(dir * angularSpeed);
+            transform.position = new Vector3(x, y, z += forwardSpeed * Time.deltaTime);
+        }
+
+        protected virtual void Update()
+        {
+            angularSpeed += Time.deltaTime * speed;
+            UpdateObject();
+        }
+
+        protected virtual void PickDirection()
+        {
+            int rand = Random.Range(0, 2);
+            switch (rand)
+            {
+                case 0:
+                    dir = -1;
+                    break;
+                case 1:
+                    dir = 1;
+                    break;
+            }
+        }
+
+        protected virtual void Initialize()
+        {
+            transform.position = Vector3.forward * 25f;
+            z = transform.position.z;
+            PickDirection();
+            render.enabled = true;
+        }
+
+        protected virtual void UpdateObject()
+        {
+            if(transform.position.z < -1f)render.enabled = false;
+            if (transform.position.z < -5) Initialize();
+        }
+
+        protected virtual void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "Player") Debug.Log("You lost");
+        }
+
+    }
+}
---
+++ Assets/Scripts/TubeRacer/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eric_Sanchez_Verges
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]float speed;
+
+        float x, y, z = 0f;
+        float angularSpeed = 0f;
+
+        Vector3 rotation;
+
+        // Update is called once per frame
+        void Update()
+        {
+            angularSpeed += Input.GetAxisRaw("Horizontal")*Time.deltaTime*speed;
+            
+        }
+
+        private void FixedUpdate()
+        {
+            x = Mathf.Cos(angularSpeed);
+            y = Mathf.Sin(angularSpeed);
+            transform.position = new Vector3(x, y, z);
+            rotation = transform.rotation.eulerAngles;
+            rotation += Vector3.forward * Input.GetAxisRaw("Horizontal") * 5.73f;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
