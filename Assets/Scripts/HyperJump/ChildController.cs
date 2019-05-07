b016ec249b47f1a5fc1184ed1632f2e4eba133a7
Eric Sanchez <esanchezverges@gmail.com>
2019-05-07 17:49:12 +0200

    Hola

--- Assets/Scripts/HyperJump/ChildController.cs
+++ Assets/Scripts/HyperJump/ChildController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class ChildController : MonoBehaviour
 {
-    bool isEnemy;
+    public bool isEnemy;
+
+    [SerializeField] float rotationSpeed;
+
+    float x, y, z;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = transform.position.x;
+        y = transform.position.y;
+        z = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(x, y, z);    
     }
 }
