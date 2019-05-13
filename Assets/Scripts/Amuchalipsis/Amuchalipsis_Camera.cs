c7bb083059aff9572780850f335e42ffd456c0b8
mario hernandez <marioherferj@gmail.com>
2019-05-13 17:37:24 +0200

    DinoControl, DinoCamera

---
+++ Assets/Scripts/Amuchalipsis/Amuchalipsis_Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amuchalipsis_Camera : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    Vector3 StartPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(Player.transform.position.x + (StartPos.x-1), 
+            Player.transform.position.y + StartPos.y, 
+            Player.transform.position.z + StartPos.z);
+    }
+}
