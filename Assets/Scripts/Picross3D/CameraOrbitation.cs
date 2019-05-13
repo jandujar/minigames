36ff26373adcc5aa62107bf49d597ba0a588e4c7
Teteju <jesusjavier.maldonado@balmes.epiaedu.cat>
2019-05-13 17:37:29 +0200

    added CameraOrbit

---
+++ Assets/Scripts/Picross3D/CameraOrbitation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbitation : MonoBehaviour
+{
+
+
+    [SerializeField] Transform CenterPoint;
+
+    float speed = 30f;
+
+    float InputAxisX, InputAxisY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON3))
+        {
+
+            InputAxisX = 0;
+            InputAxisY = 0;
+            InputAxisX = InputManager.Instance.GetAxisHorizontal();
+            InputAxisY = InputManager.Instance.GetAxisVertical();
+            transform.Rotate(Time.deltaTime * (InputAxisY*speed*-1), Time.deltaTime * (InputAxisX * speed*-1), 0);
+        }
+
+        if (InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON1))
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+
+    }
+}
