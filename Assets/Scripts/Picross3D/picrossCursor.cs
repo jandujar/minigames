51025632ce87f87e65b980c9117f645e3c1992e5
Laura Romo <laura_romo_89@hotmail.es>
2019-05-13 17:51:27 +0200

    Arreglado movimiento de cursor 2.0

--- Assets/Scripts/Picross3D/picrossCursor.cs
+++ Assets/Scripts/Picross3D/picrossCursor.cs
@@ -10,91 +10,23 @@ public class picrossCursor : MonoBehaviour {
     bool down;
     bool tope;
     bool shoot;
+    float speed;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start() {
         right = false;
         left = false;
         tope = false;
         shoot = false;
+        speed = 0.01f;
     }
 
     // Update is called once per frame
     void Update() {
+        horizontal = speed * InputManager.Instance.GetAxisHorizontal();
+        vertical = speed * InputManager.Instance.GetAxisVertical();
 
-        // Does the ray intersect any objects excluding the player layer
-        /*if (shoot) {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Did Hit " + hit.collider.gameObject);
-            }
-            else {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-                Debug.Log("Did not Hit");
-            }
-            //shoot = false;
-        }*/
-
-        //Movimientos
-        if(InputManager.Instance.GetAxisHorizontal() > 0) {
-            if (InputManager.Instance.GetAxisHorizontal() == 1) {
-                left = true;
-                tope = true;
-            }
-            if (left) {
-                Debug.Log("LLegue al tope");
-                transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
-            }
-        }
-
-        else if (InputManager.Instance.GetAxisHorizontal() < 0) {
-            if (InputManager.Instance.GetAxisHorizontal() == -1) {
-                right = true;
-                tope = true;
-            }
-            if (right) {
-                transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
-                Debug.Log("Move cursor right");
-            }
-        }
-
-        if (InputManager.Instance.GetAxisVertical() > 0) {
-            if (InputManager.Instance.GetAxisVertical() == 1) {
-                up = true;
-                tope = true;
-            }
-            if (up) {
-                transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
-            }
-        }
-
-        else if (InputManager.Instance.GetAxisVertical() < 0) {
-            if (InputManager.Instance.GetAxisVertical() == -1) {
-                down = true;
-                tope = true;
-            }
-            if (down) {
-                transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
-                Debug.Log("Move cursor right");
-            }
-        }
-
-        if (InputManager.Instance.GetAxisHorizontal() < 1 && left) {
-            left = false;
-        }
-
-        if (InputManager.Instance.GetAxisHorizontal() > -1 && right) {
-            right = false;
-        }
-
-        if (InputManager.Instance.GetAxisVertical() < 1 && up) {
-            up = false;
-        }
-
-        if (InputManager.Instance.GetAxisVertical() > -1 && down) {
-            down = false;
-        }
-
-        
+        transform.position = new Vector3(transform.position.x + horizontal, transform.position.y + vertical, transform.position.z);
     }
 }
