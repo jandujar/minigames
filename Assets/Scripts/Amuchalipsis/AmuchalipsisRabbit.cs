ecdbf78b3cd8254b3d12ea5575cc82cf0abc7f18
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2019-05-13 17:13:01 +0200

    DinoRabbit

--- Assets/Scripts/Amuchalipsis/AmuchalipsisRabbit.cs
+++ Assets/Scripts/Amuchalipsis/AmuchalipsisRabbit.cs
@@ -1,39 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace ivan_alvarez_enri
+{
 public class AmuchalipsisRabbit : MonoBehaviour
 {  
     public GameObject Blood;
     public GameObject Body;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private float VerticalInput=0F;
+    private float HorizontalInput=0F;
+    private bool dead=false;
 
     // Update is called once per frame
+    private void Start() {
+        InvokeRepeating("ChooseNewInputs",1F,5F);
+    }
     void Update()
     {
 
-        // if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
-        //     Die();
-        if(InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON1)){
-            gameObject.GetComponent<Rigidbody>().AddExplosionForce(5F,gameObject.transform.position-new Vector3(1,-0.5F,0),10,0,ForceMode.Force);
-            Debug.Log("moving");
-        }
-        if(InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON2)){
-            gameObject.GetComponent<Rigidbody>().AddExplosionForce(5F,gameObject.transform.position-new Vector3(-1,-0.5F,0),10,0,ForceMode.Force);
-            Debug.Log("moving");
-        }
-        if(InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON3)){
-            gameObject.GetComponent<Rigidbody>().AddExplosionForce(5F,gameObject.transform.position-new Vector3(0,-0.5F,1),10,0,ForceMode.Force);
-            Debug.Log("moving");
-        }
-        if(InputManager.Instance.GetButton(InputManager.MiniGameButtons.BUTTON4)){
-            gameObject.GetComponent<Rigidbody>().AddExplosionForce(5F,gameObject.transform.position-new Vector3(0,-0.5F,-1),10,0,ForceMode.Force);
-            Debug.Log("moving");
-        }
+         if(InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1))
+             Die();
+        if(!dead)
+        gameObject.GetComponent<Rigidbody>().AddExplosionForce(5F,gameObject.transform.position-new Vector3(VerticalInput,-0.5F,HorizontalInput),10,0,ForceMode.Force);
+            //Debug.Log("moving");
+       
     }
     public void Die(){
         Rigidbody[] BloodBoxParticles=Blood.GetComponentsInChildren<Rigidbody>();
@@ -43,12 +34,28 @@ public class AmuchalipsisRabbit : MonoBehaviour
             BloodBox.AddExplosionForce(10,(BloodBox.gameObject.transform.position-new Vector3(Random.Range(-1F,1F),5,Random.Range(-1F,1F))),10F,1F,ForceMode.Impulse);
            
         }
+        Instantiate(Resources.Load("Amuchalipsis/BloodPart") as GameObject,gameObject.transform.position-new Vector3(0,0.2F,0),new Quaternion());
         gameObject.GetComponent<BoxCollider>().enabled=false;
         gameObject.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.FreezeAll;
-        Body.SetActive(false);
+        //gameObject.GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezePositionY;
+        Body.transform.localScale=new Vector3(1,0.1f,1);
+        Body.transform.localPosition= new Vector3(0,0.01F,0);
+        gameObject.transform.rotation=new Quaternion();
+        dead=true;
         //gameObject.transform.localScale=new Vector3(1,0.1F,1);
 
         //gameObject.GetComponent<Rigidbody>().AddForce(Random.Range(-5F,5F),10,0,ForceMode.Impulse);
         
     }
+    private void ChooseNewInputs(){
+        VerticalInput=Random.Range(-1,2);
+        HorizontalInput=Random.Range(-1,2);
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag=="Player"){
+            Die();
+        }
+    }
+}
 }
