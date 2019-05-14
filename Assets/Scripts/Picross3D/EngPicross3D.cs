3617cde0243bbd994ae634726b09a5035e29df11
Teteju <jesusjavier.maldonado@balmes.epiaedu.cat>
2019-05-14 16:04:24 +0200

    added camera orbitation with errors

---
+++ Assets/Scripts/Picross3D/EngPicross3D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngPicross3D : MonoBehaviour
+{
+
+    [SerializeField] float maxlifes;
+
+    private float lifes;
+
+    private float badCubesNum;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (maxlifes <= 0) maxlifes = 3;
+        lifes = maxlifes;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
