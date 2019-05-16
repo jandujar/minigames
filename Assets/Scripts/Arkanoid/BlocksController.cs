2963295d4d03cda8fe5297ac0bbe7076ef583556
joan bogunya <joanbog97@gmail.com>
2019-05-16 12:18:14 +0200

    change directionBall and change block color

---
+++ Assets/Scripts/Arkanoid/BlocksController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocksController : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        spriteRenderer.color = Random.ColorHSV();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.name == "Ball")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Arkanoid/PaleteController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaleteController : MonoBehaviour
+{
+    private float move;
+    public float velocityX;
+    private Vector3 ActualPosition;
+    public float maxPositionX;
+    public GameObject ball;
+    public int ballSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = InputManager.Instance.GetAxisHorizontal();
+
+        transform.Translate(move * velocityX * Time.deltaTime, 0, 0);
+
+       if(transform.position.x > maxPositionX)
+        {
+            ActualPosition = new Vector3(maxPositionX, transform.position.y, transform.position.z);
+            transform.position = ActualPosition;
+        }
+
+       if(transform.position.x < -maxPositionX)
+        {
+            ActualPosition = new Vector3(-maxPositionX, transform.position.y, transform.position.z);
+            transform.position = ActualPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f,Vector2.up.y * ballSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
