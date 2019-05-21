296ac834b745081460150e478ff6f4e94f8a2068
Eric Sanchez <esanchezverges@gmail.com>
2019-05-21 15:30:01 +0200

    Salto hecho

--- Assets/Scripts/HyperJump/BallController.cs
+++ Assets/Scripts/HyperJump/BallController.cs
@@ -8,20 +8,70 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public float breakVelocity;
+    RaycastHit hit;
+    Ray ray ;
+    public float jumpForce;
+    Rigidbody rb;
+    
 
+    private void Start()
+    {  
+        rb = GetComponent<Rigidbody>();
+    }
     private void Update()
     {
-        Debug.Log(GetComponent<Rigidbody>().velocity);
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.GetComponent<MeshRenderer>().material.color == Color.red)
+        ray = new Ray(transform.position, Vector3.down);
+        Physics.Raycast(ray, out hit, Mathf.Infinity);
+        if (hit.distance <= 0.30f)
         {
-            Debug.Log("Game Over");
-            if(GetComponent<Rigidbody>().velocity.z > breakVelocity)
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+    
+    
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    try
+    //    {
+    //        if (collision.gameObject.GetComponentInParent<ObstacleController>().isEnemy)
+    //        {
+                
+    //            if (rb.velocity.y > breakVelocity)
+    //            {
+    //                collision.gameObject.GetComponentInParent<ObstacleController>().Shoot();
+    //                collision.gameObject.transform.parent.GetComponentInChildren<Collider>().enabled = false;
+    //                Debug.Log("I tried");
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("GameOver");
+    //            }
+    //        }
+    //    }
+    //    catch { }
+       
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        try
+        {
+            if (other.gameObject.GetComponentInParent<ObstacleController>().isEnemy)
             {
-                collision.transform.parent.gameObject.GetComponent<ObstacleController>().Shoot();
+
+                if (rb.velocity.y > breakVelocity)
+                {
+                    other.gameObject.GetComponentInParent<ObstacleController>().Shoot();
+                    other.gameObject.transform.parent.GetComponentInChildren<Collider>().enabled = false;
+                    Debug.Log("I tried");
+                }
+                else
+                {
+                    Debug.Log("GameOver");
+                }
             }
         }
+        catch { }
     }
 }
