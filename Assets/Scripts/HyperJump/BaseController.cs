f4c617265cbe15d8408e22adb0daec0508c4aad4
Eric Sanchez <esanchezverges@gmail.com>
2019-05-28 16:03:16 +0200

    Controles ahora si?

--- Assets/Scripts/HyperJump/BaseController.cs
+++ Assets/Scripts/HyperJump/BaseController.cs
@@ -17,9 +17,7 @@ namespace HyperJump
         // Update is called once per frame
         void Update()
         {
-            if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON1)) axis = 1;
-            else if (InputManager.Instance.GetButtonDown(InputManager.MiniGameButtons.BUTTON2)) axis = -1;
-            else axis = 0;
+            axis = InputManager.Instance.GetAxisHorizontal();
         }
 
         void FixedUpdate()
