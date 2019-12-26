4bad8364dcdab680f29d4d7e0fb4b1dfc57ae5a9
Netherion <97quesada@gmail.com>
2019-12-26 11:35:33 +0100

    Pruebas varias de movimiento de coche

--- Assets/Scripts/RacingCars/CocheEnemigo.cs
+++ Assets/Scripts/RacingCars/CocheEnemigo.cs
@@ -7,6 +7,10 @@ public class CocheEnemigo : MonoBehaviour
     [SerializeField] private float speed = 0f;
     private GameManager gameManager;
 
+    public BoxCollider frenar;
+    public BoxCollider giroIzq;
+    public BoxCollider giroDer;
+
     public bool enableCar = false;
 
     public void init(GameManager gm)
@@ -14,16 +18,26 @@ public class CocheEnemigo : MonoBehaviour
         gameManager = gm;
     }
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if (enableCar)
         {
-            transform.Translate(0f, speed * Time.deltaTime, 0f);
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+
+        Ray ray = new Ray(transform.position, transform.up);
+        RaycastHit hit;
+        if (frenar.Raycast(ray, out hit, 5f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 1000, Color.white);
+            Debug.Log("Did not Hit");
         }
     }
+
+
 }
