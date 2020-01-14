388bafa4c10bc8ea60fedd31b02e66cb11f94f61
Arnau_merin <arnaumerin@gmail.com>
2020-01-14 12:05:02 +0100

    Todo el proyecto en un Push como un campeon

---
+++ Assets/Scripts/Fishing/Scripts/TankShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulledPrefab;
+
+    public float bulledSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space")){
+            Shoot();
+            //Debug.Log("PIUM");
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject Bulled = Instantiate(bulledPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = Bulled.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulledSpeed, ForceMode2D.Impulse);
+    }
+}
