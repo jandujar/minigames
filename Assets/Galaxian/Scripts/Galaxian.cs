401a38067de37943c6a23dca8f505c571c59e1cf
Netherion <97quesada@gmail.com>
2020-01-14 17:11:40 +0100

    Añadir carpeta Galaxian con mierdas varias

---
+++ Assets/Galaxian/Scripts/Galaxian.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Galaxian : IMiniGame
+{
+    public NaveGalaxian nave;
+
+    void Awake()
+    {
+        //Init Juego
+    }
+
+    public override void beginGame()
+    {
+        Debug.Log(this.ToString() + " game Begin");
+        nave.enableNave = true;
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        nave.init(gm);
+    }
+
+    public override string ToString()
+    {
+        return "Galaxian";
+    }
+}
