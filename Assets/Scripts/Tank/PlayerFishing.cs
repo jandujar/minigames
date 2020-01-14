b5a964ff33154fad12c1ffaa33fc8b9c8031f90c
Esiier <essiieer@hotmail.com>
2020-01-14 11:58:19 +0100

    Tank Fish game base

---
+++ Assets/Scripts/Tank/PlayerFishing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFishing : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+    }
+}
