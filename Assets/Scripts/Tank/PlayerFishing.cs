abab3fe9bfa49e71aff65be7e98ce49fd1c264fe
Esiier <essiieer@hotmail.com>
2020-01-14 13:20:45 +0100

    Terminado TankFish

--- Assets/Scripts/Tank/PlayerFishing.cs
+++ Assets/Scripts/Tank/PlayerFishing.cs
@@ -10,14 +10,30 @@ public class PlayerFishing : MonoBehaviour
 
     Vector2 movement;
 
+    public int totalFish = 7;
+    public int lives = 1;
+
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
     }
 
     void FixedUpdate(){
         rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Fish"){
+            totalFish--;
+            Debug.Log(totalFish);
+        }
+        if (collision.gameObject.tag == "EnemyShip"){
+            lives--;
+            Debug.Log(lives);
+        }
     }
 }
