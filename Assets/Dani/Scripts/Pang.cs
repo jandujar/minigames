819c01e19631de67566f1d5c78b0ac31ae973b2b
Dgpes <oportunitatzero@gmail.com>
2020-01-14 17:12:38 +0100

    Pang pull request

---
+++ Assets/Dani/Scripts/Pang.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pang : IMiniGame
+{
+    public GameManager gameManager;
+    public GameObject game;
+    public override void beginGame()
+    {
+
+    }
+
+    public override void initGame(MiniGameDificulty difficulty, GameManager gm)
+    {
+        this.gameManager = gm;
+    }
+    public void EndGame(bool win)
+    {
+        if (win)
+        {
+            gameManager.EndGame(IMiniGame.MiniGameResult.WIN);
+        }
+        else
+        {
+            gameManager.EndGame(IMiniGame.MiniGameResult.LOSE);
+        }
+    }
+
+}
